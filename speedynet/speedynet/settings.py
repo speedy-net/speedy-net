b7eb24aa74ac54564b180a0207bcb421a55592f9
Ubuntu <ubuntu@staging.speedy2000.net>
2015-12-03 10:55:07 +0000

    add gitignore, remvoe conf

--- speedynet/speedynet/settings.py
+++ speedynet/speedynet/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7@kn+@3ze*#je60s+x+#@79e5(#p935v3*72&(r&$_z^l)xtn)'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
