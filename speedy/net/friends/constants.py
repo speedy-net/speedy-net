b3ad9fcb72d399accbd903468c569700664a0ad5
Uri <uri@speedy.net>
2016-08-08 20:22:33 +0300

    Added ~~~~ TODO

--- speedy/net/friends/constants.py
+++ speedy/net/friends/constants.py
@@ -1,3 +1,3 @@
-# MAXIMUM_NUMBER_OF_FRIENDS_ALLOWED = 800
-MAXIMUM_NUMBER_OF_FRIENDS_ALLOWED = 3 # For testing when there are close to MAXIMUM_NUMBER_OF_FRIENDS_ALLOWED friends.
+# MAXIMUM_NUMBER_OF_FRIENDS_ALLOWED = 800 ~~~~ TODO: uncomment this line!
+MAXIMUM_NUMBER_OF_FRIENDS_ALLOWED = 3 # For testing when there are close to MAXIMUM_NUMBER_OF_FRIENDS_ALLOWED friends. ~~~~ TODO: remove this line!
 
