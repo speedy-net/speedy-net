921e48c5e578309e5694fd33f0fe697921dbdf77
Evgeniy Kirov <evgeniy.kirov@initech.co.il>
2016-08-11 15:27:51 +0300

    Added slug and username to UserFactory

--- speedy/net/accounts/test_factories.py
+++ speedy/net/accounts/test_factories.py
@@ -1,4 +1,7 @@
+import string
+
 import factory
+import factory.fuzzy
 
 from .models import User, UserEmailAddress
 
@@ -7,6 +10,8 @@ class UserFactory(factory.DjangoModelFactory):
     first_name = factory.Faker('first_name')
     last_name = factory.Faker('last_name')
     gender = User.GENDER_OTHER
+    slug = factory.fuzzy.FuzzyText(chars=string.ascii_lowercase)
+    username = factory.LazyAttribute(lambda o: o.slug)
 
     password = factory.PostGenerationMethodCall('set_password', '111')
 
