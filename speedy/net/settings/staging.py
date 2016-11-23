3d0e28c8ff385bdc72412a6d9dbf6cca9f0f9df9
Evgeniy Kirov <evgeniy.kirov@initech.co.il>
2016-11-23 13:58:46 +0300

    Changed settings.MANAGERS

--- speedy/net/settings/staging.py
+++ speedy/net/settings/staging.py
@@ -3,6 +3,6 @@ from .base_site import *
 DEFAULT_FROM_EMAIL = 'webmaster@speedy2000.net'
 
 ADMINS = MANAGERS = (
-    ('Uri Even-Chen', 'uri@speedy.net'),
+    ('Uri Even-Chen', 'webmaster@speedy.net'),
     ('Evgeniy Kirov', 'evgeniy.kirov@initech.co.il'),
 )
