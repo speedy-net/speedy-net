91fa0ce487b61b987896ee05df325229ca2bf309
Evgeniy Kirov <evgeniy.kirov@initech.co.il>
2016-11-30 13:56:53 +0300

    Updated nginx and uwsgi configs
    [Speedy Net] Speedy Net websites
    https://trello.com/c/582ef73e2722e95bedaf90bb

--- contrib/deploy.sh
+++ contrib/deploy.sh
@@ -3,17 +3,16 @@
 DIR=/home/ubuntu/speedy-net
 PY=$DIR/env/bin/python
 PIP=$DIR/env/bin/pip
+SITES="net match composer mail"
 
 cd $DIR
 git pull
 $PIP install -r requirements.txt
 
-cd $DIR/speedy/net
-$PY manage.py migrate
-$PY manage.py collectstatic --no-input
-touch /run/uwsgi/app/speedy_net/reload
-
-cd $DIR/speedy/match
-$PY manage.py migrate
-$PY manage.py collectstatic --no-input
-touch /run/uwsgi/app/speedy_match/reload
+for site in $SITES
+do
+    cd "$DIR/speedy/$site"
+    $PY manage.py migrate
+    $PY manage.py collectstatic --no-input
+    touch "/run/uwsgi/app/speedy_$site/reload"
+done
