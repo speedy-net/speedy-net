8a639de4ba2d6becc779ba55e0d0feeee34c3c97
Evgeniy Kirov <evgeniy.kirov@initech.co.il>
2016-12-01 19:30:58 +0300

    Added cache config

--- speedy/net/settings/base.py
+++ speedy/net/settings/base.py
@@ -113,6 +113,10 @@ DATABASES = {
     'default': env.db()
 }
 
+CACHES = {
+    'default': env.cache()
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
