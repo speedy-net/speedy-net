e421621fa9c5d32acfce528ef9bc87671f445dd3
Uri <uri@speedy.net>
2016-12-07 13:46:38 +0200

    Updated staging.py.

--- speedy/composer/settings/staging.py
+++ speedy/composer/settings/staging.py
@@ -1,3 +1,8 @@
 from .base_site import *
 
 DEFAULT_FROM_EMAIL = 'webmaster@speedy2000.net'
+
+ADMINS = MANAGERS = (
+    ('Uri Even-Chen', 'webmaster@speedy.net'),
+    ('Evgeniy Kirov', 'evgeniy.kirov@initech.co.il'),
+)
