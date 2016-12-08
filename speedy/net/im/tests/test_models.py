eb8cb4f67aa7fd44352f53be19f760b30c8efd61
Evgeniy Kirov <evgeniy.kirov@initech.co.il>
2016-12-08 13:59:42 +0300

    #582dc528a8799b7fc7d7b0aa added speedy.net.im.tests.test_models.ChatTestCase.test_get_slug https://trello.com/c/582dc528a8799b7fc7d7b0aa

--- speedy/net/im/tests/test_models.py
+++ speedy/net/im/tests/test_models.py
@@ -15,3 +15,12 @@ class ChatTestCase(TestCase):
         chat = ChatFactory(ent1=UserFactory(first_name='Walter', last_name='White'),
                            ent2=UserFactory(first_name='Jesse', last_name='Pinkman'))
         self.assertEqual(first=str(chat), second='Walter White, Jesse Pinkman')
+
+    def test_get_slug(self):
+        user1=UserFactory(first_name='Walter', last_name='White', slug='walter')
+        user2=UserFactory(first_name='Jesse', last_name='Pinkman', slug='jesse')
+        chat = ChatFactory(ent1=user1, ent2=user2)
+        self.assertEqual(first=chat.get_slug(current_user=user1), second='jesse')
+        self.assertEqual(first=chat.get_slug(current_user=user2), second='walter')
+        chat = ChatFactory(ent1=None, ent2=None, is_group=True, group=[user1, user2, UserFactory(), UserFactory()])
+        self.assertEqual(first=chat.get_slug(current_user=user1), second=chat.id)
