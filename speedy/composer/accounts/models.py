1a9d6fe85b6d051bde890b719a2d915d088365ae
Uri <uri@speedy.net>
2016-12-15 15:37:14 +0200

    Changed Speedy Composer models - not ready to use them yet - under construction.

--- speedy/composer/accounts/models.py
+++ speedy/composer/accounts/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from speedy.core.models import TimeStampedModel
 from speedy.net.accounts.models import NamedEntity, SiteProfileBase
 
 
-class SpeedyComposerNamedEntity(NamedEntity):
+# ~~~~ TODO: Create a node in speedy.core which will be used in all Speedy Net websites. This node should contain a username (which is the slug without dashes), slug and name. Also check that the full path after the domain name is unique in each website. But it doesn't have to be unique across different websites (such as Speedy Net and Speedy Composer).
+# ~~~~ TODO: This node should be a base for each website's node. For example in Speedy Net nodes can be albums and photos. Each node will have a unique URL, albums will start with the user/page or an entity's slug and then the album's slug (which is unique only per entity) and a photo's URL will be the album's URL + the photo's slug (which will be unique per album). A photo must be linked to exactly one album.
+# ~~~~ TODO: https://trello.com/c/gaKvb9eG/4-fix-model-hierarchy-and-speedy-composer-tests
+class SpeedyComposerNode(TimeStampedModel): # ~~~~ TODO: check which class we want to inherit from?
     MIN_USERNAME_LENGTH = 1
     MAX_USERNAME_LENGTH = 200
     MIN_SLUG_LENGTH = 1
@@ -16,7 +20,7 @@ class SpeedyComposerNamedEntity(NamedEntity):
         abstract = True
 
     def __str__(self):
-        return '{}'.format(self.name)
+        return '{}'.format(self.name) # ~~~~ TODO: fix!
 
 
 class SiteProfile(SiteProfileBase):
--- speedy/composer/compose/models.py
+++ speedy/composer/compose/models.py
@@ -2,24 +2,24 @@ from django.conf import settings
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from speedy.composer.accounts.models import SpeedyComposerNamedEntity
+from speedy.composer.accounts.models import SpeedyComposerNode
 
 
-class ChordsTemplate(SpeedyComposerNamedEntity):
+class ChordsTemplate(SpeedyComposerNode):
 
     class Meta:
         verbose_name = _('chords template')
         verbose_name_plural = _('chords templates')
 
 
-class Accompaniment(SpeedyComposerNamedEntity):
+class Accompaniment(SpeedyComposerNode):
 
     class Meta:
         verbose_name = _('accompaniment')
         verbose_name_plural = _('accompaniments')
 
 
-class Folder(SpeedyComposerNamedEntity):
+class Folder(SpeedyComposerNode):
     user = models.ForeignKey(verbose_name=_('user'), to=settings.AUTH_USER_MODEL, related_name='+')
 
     class Meta:
@@ -27,7 +27,7 @@ class Folder(SpeedyComposerNamedEntity):
         verbose_name_plural = _('folders')
 
 
-class Composition(SpeedyComposerNamedEntity):
+class Composition(SpeedyComposerNode):
     folder = models.ForeignKey(verbose_name=_('folder'), to=Folder, related_name='+')
     chords_template = models.ForeignKey(verbose_name=_('chords template'), to=ChordsTemplate, related_name='+')
     accompaniment = models.ForeignKey(verbose_name=_('accompaniment'), to=Accompaniment, related_name='+')
