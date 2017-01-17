8963c80162263e937c157d89267513388431bc3d
Gleb <gleb.tocarenco@gmail.com>
2017-01-17 18:06:26 +0200

    changes APP DIR

--- speedy/net/settings/base_site.py
+++ speedy/net/settings/base_site.py
@@ -1,6 +1,7 @@
 from django.utils.translation import ugettext_lazy as _
 from speedy.core.settings.base import *
 from speedy.core.settings.base_site import update_site_paths
+from .utils import APP_DIR
 
 update_site_paths(settings=globals())
 
