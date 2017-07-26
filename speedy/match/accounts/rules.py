16c74a52c33965578c72a3db433e880eed6c84d2
Uri <uri@speedy.net>
2017-07-26 14:16:14 +0300

    Updated rules - I will be able to view a user I blocked.

--- speedy/match/accounts/rules.py
+++ speedy/match/accounts/rules.py
@@ -4,6 +4,7 @@ from rules import predicate, add_perm, remove_perm, always_allow
 
 from .models import SiteProfile
 from speedy.core.im.models import Chat
+from speedy.core.blocks.models import Block
 from speedy.match.likes.models import UserLike
 from speedy.core.blocks.rules import is_blocked, there_is_block
 from speedy.core.accounts.rules import has_access_perm
@@ -14,8 +15,9 @@ def is_match_profile(user, other):
     if user.is_authenticated():
         match_profile = user.profile.get_matching_rank(other_profile=other.profile) > SiteProfile.RANK_0
         has_message = Chat.on_site.filter((Q(ent1_id=user) & Q(ent2_id=other)) | (Q(ent1_id=other) & Q(ent2_id=user))).exists()
-        has_likes = UserLike.objects.filter((Q(from_user=user) & Q(to_user=other)) | (Q(from_user=other) & Q(to_user=user)))
-        return (user == other) or (match_profile or has_message or has_likes)
+        has_likes = UserLike.objects.filter((Q(from_user=user) & Q(to_user=other)) | (Q(from_user=other) & Q(to_user=user))).exists()
+        has_blocked = Block.objects.has_blocked(blocker=user, blockee=other)
+        return (user == other) or (match_profile or has_message or has_likes or has_blocked)
     return False
 
 
