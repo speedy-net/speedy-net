4ddf0279d0cad0d2f9d9e63a816159e103b709aa
Uri <uri@speedy.net>
2017-07-27 14:04:22 +0300

    import env from speedy.core and not speedy.net.

--- speedy/match/manage.py
+++ speedy/match/manage.py
@@ -4,7 +4,7 @@ import sys
 from pathlib import Path
 sys.path.insert(0, str(Path(__file__).absolute().parent.parent.parent))
 
-from speedy.net.settings.utils import env
+from speedy.core.settings.utils import env
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "speedy.match.settings.{}".format(env('ENVIRONMENT')))
