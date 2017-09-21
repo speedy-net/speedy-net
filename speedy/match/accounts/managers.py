a64423a8a9e59bb0954652af05fff156033633a9
Evgeniy Kirov <evg.kirov@gmail.com>
2017-09-21 18:58:54 +0300

    [Speedy Match] Display matching rank in /matches/ https://trello.com/c/58fc719d7d54db9cdc6cb112

--- speedy/match/accounts/managers.py
+++ speedy/match/accounts/managers.py
@@ -7,7 +7,7 @@ from speedy.core.base.utils import get_age_ranges_match
 class SiteProfileManager(models.Manager):
     def get_matches(self, user_profile):
         age_ranges = get_age_ranges_match(user_profile.min_age_match, user_profile.max_age_match)
-        qs = User.objects.active(gender__in=user_profile.gender_to_match, date_of_birth__range=age_ranges).exclude(pk=user_profile.user_id)
+        qs = User.objects.active(gender__in=user_profile.gender_to_match, date_of_birth__range=age_ranges).exclude(pk=user_profile.user_id).distinct()
 
         qs = [user for user in qs if (user_profile.get_matching_rank(other_profile=user.profile) > self.model.RANK_0) and user.profile.is_active]
 
