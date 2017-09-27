3c7f15dea35ce3bf2aef7b870b1034d040e6de5b
Uri <uri@speedy.net>
2017-09-27 13:32:39 +0300

    Added Asaf Gery to staging admins.

--- speedy/core/settings/staging.py
+++ speedy/core/settings/staging.py
@@ -4,7 +4,7 @@ from .base_site import *
 def activate_staging(settings):
     admins = (
         ('Uri Even-Chen', 'webmaster@speedy.net'),
-        ('Evgeniy Kirov', 'evgeniy.kirov@initech.co.il'),
+        ('Asaf Gery', 'asaf.gery@gmail.com'),
     )
     settings.update({
         'DEFAULT_FROM_EMAIL': 'webmaster@speedy2000.net',
