67d61e493b9608ef1164374938ca9242fb245eb8
Uri <uri@speedy.net>
2017-12-25 18:36:41 +0200

    Revert "Added Asaf Gery to staging admins."
    This reverts commit 3c7f15dea35ce3bf2aef7b870b1034d040e6de5b.

--- speedy/core/settings/staging.py
+++ speedy/core/settings/staging.py
@@ -4,7 +4,7 @@ from .base_site import *
 def activate_staging(settings):
     admins = (
         ('Uri Even-Chen', 'webmaster@speedy.net'),
-        ('Asaf Gery', 'asaf.gery@gmail.com'),
+        ('Evgeniy Kirov', 'evgeniy.kirov@initech.co.il'),
     )
     settings.update({
         'DEFAULT_FROM_EMAIL': 'webmaster@speedy2000.net',
