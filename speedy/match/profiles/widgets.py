37a9b4c76deff534aa4dfa5a92844a82a32823dd
Evgeniy Kirov <evgeniy.kirov@initech.co.il>
2017-12-28 19:40:47 +0300

    126 - A Speedy Net profile should have a link to the relevant Speedy Match profile.

--- speedy/match/profiles/widgets.py
+++ speedy/match/profiles/widgets.py
@@ -1,14 +1,10 @@
-import logging
-
 from django.conf import settings
-from django.contrib.auth.models import AnonymousUser
 from django.contrib.sites.models import Site
 from django.utils.translation import get_language
 
 from speedy.core.profiles.widgets import Widget
 from speedy.match.accounts.models import SiteProfile as MatchSiteProfile
 
-log = logging.getLogger('django') #TODO: find out why no logging is happening
 
 class UserOnSpeedyMatchWidget(Widget):
     template_name = 'profiles/user_on_speedy_match_widget.html'
@@ -18,23 +14,23 @@ class UserOnSpeedyMatchWidget(Widget):
         return self.entity.get_profile(model=MatchSiteProfile)
 
     def get_viewer_speedy_match_profile(self):
-        if self.viewer.__class__ == AnonymousUser:
+        if not self.viewer.is_authenticated:
             return None
-        else:
-            return self.viewer.get_profile(model=MatchSiteProfile)
+        return self.viewer.get_profile(model=MatchSiteProfile)
 
     def is_match(self):
-        if self.viewer.__class__ == AnonymousUser:
+        if not self.viewer.is_authenticated:
             return False
-        else:
-            return self.get_entity_speedy_match_profile().get_matching_rank(self.get_viewer_speedy_match_profile()) > MatchSiteProfile.RANK_0
+        if self.viewer == self.entity:
+            return False
+        return self.get_entity_speedy_match_profile().get_matching_rank(self.get_viewer_speedy_match_profile()) > MatchSiteProfile.RANK_0
 
     def get_context_data(self):
         cd = super().get_context_data()
         language_code = get_language()
         SPEEDY_MATCH_SITE_ID = settings.SITE_PROFILES['match']['site_id']
         cd.update({
-            'matches': self.is_match(),
+            'is_match': self.is_match(),
             'speedy_match_url': Site.objects.get(id=SPEEDY_MATCH_SITE_ID).domain,
             'LANGUAGE_CODE': language_code
         })
