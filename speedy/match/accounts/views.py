650c103803c018d0a4d0e27ce6ffff1ffabde3f8
Evgeniy Kirov <evgeniy.kirov@initech.co.il>
2018-01-11 19:18:13 +0300

    [Speedy Match] 99 - Activation form fix 2

--- speedy/match/accounts/views.py
+++ speedy/match/accounts/views.py
@@ -37,9 +37,6 @@ class ActivateSiteProfileView(CoreActivateSiteProfileView):
         log.debug('HERE: get_form_kwargs')
         kwargs = super().get_form_kwargs()
         kwargs['step'] = self.step
-        if 'gender_to_match' in self.request.POST:
-            kwargs['data'] = kwargs['data'].copy()
-            kwargs['data']['gender_to_match'] = ','.join(self.request.POST.getlist('gender_to_match'))
         return kwargs
 
     #TODO: Read about dispatch and request lifecycle to understand why 'step' is wrong
