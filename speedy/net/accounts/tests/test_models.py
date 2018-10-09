41809556f13de4ae0a53f52c675bd9b42a4876dd
Uri <uri@speedy.net>
2018-10-09 19:09:21 +0300

    Updated tests.

--- speedy/net/accounts/tests/test_models.py
+++ speedy/net/accounts/tests/test_models.py
@@ -6,24 +6,25 @@ from speedy.core.accounts.tests.test_factories import DefaultUserFactory, Inacti
 
 
 @only_on_speedy_net
-class SiteProfileTestCase(TestCase):
+class SpeedyNetSiteProfileTestCase(TestCase):
     def get_default_user_1(self):
-        user_1 = InactiveUserFactory(first_name='Jesse', last_name='Pinkman', slug='jesse-pinkman', date_of_birth=datetime(year=1978, month=9, day=12), gender=User.GENDER_FEMALE, diet=User.DIET_VEGAN)
-        user_1.save()
-        return user_1
+        user = DefaultUserFactory(first_name='Jesse', last_name='Pinkman', slug='jesse-pinkman', date_of_birth=datetime(year=1978, month=9, day=12), gender=User.GENDER_FEMALE)
+        user.save_user_and_profile()
+        return user
 
     def get_default_user_2(self):
-        user_2 = DefaultUserFactory(first_name='Jesse', last_name='Pinkman', slug='jesse-pinkman', date_of_birth=datetime(year=1978, month=9, day=12), gender=User.GENDER_FEMALE, diet=User.DIET_VEGAN)
-        return user_2
+        user = InactiveUserFactory(first_name='Jesse', last_name='Pinkman', slug='jesse-pinkman', date_of_birth=datetime(year=1978, month=9, day=12), gender=User.GENDER_FEMALE)
+        user.save_user_and_profile()
+        return user
 
     def test_call_activate_directly_and_assert_no_exception(self):
-        user_1 = self.get_default_user_1()
+        user_1 = self.get_default_user_2()
         self.assertEqual(first=user_1.profile.is_active, second=False)
         user_1.profile.activate()
         self.assertEqual(first=user_1.profile.is_active, second=True)
 
     def test_call_deactivate_directly_and_assert_no_exception(self):
-        user_1 = self.get_default_user_2()
+        user_1 = self.get_default_user_1()
         self.assertEqual(first=user_1.profile.is_active, second=True)
         user_1.profile.deactivate()
         self.assertEqual(first=user_1.profile.is_active, second=False)
