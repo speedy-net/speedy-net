2a7367455318639b79492395bce5aa9dac56046d
Uri <uri@speedy.net>
2018-12-26 10:19:05 +0200

    Updated comments.

--- speedy/core/im/templatetags/im_tags.py
+++ speedy/core/im/templatetags/im_tags.py
@@ -8,8 +8,8 @@ from ..models import ReadMark, Chat
 @register.simple_tag
 def get_other_participant(chat, user):
     """
-    :type chat: speedy.net.im.models.Chat
-    :type user: speedy.net.accounts.models.User
+    :type chat: speedy.core.im.models.Chat
+    :type user: speedy.core.accounts.models.User
     """
     assert chat.is_private
     return chat.get_other_participants(user)[0]
@@ -18,8 +18,8 @@ def get_other_participant(chat, user):
 @register.simple_tag
 def annotate_chats_with_read_marks(chat_list, entity):
     """
-    :type chat_list: [speedy.net.im.models.Chat]
-    :type entity: speedy.net.accounts.models.Entity
+    :type chat_list: [speedy.core.im.models.Chat]
+    :type entity: speedy.core.accounts.models.Entity
     """
     rmarks = {rmark.chat_id: rmark for rmark in ReadMark.objects.filter(chat__in=chat_list, entity=entity)}
     for chat in chat_list:
@@ -36,8 +36,8 @@ def annotate_chats_with_read_marks(chat_list, entity):
 @register.simple_tag
 def annotate_messages_with_read_marks(message_list, entity):
     """
-    :type message_list: [speedy.net.im.models.Message]
-    :type entity: speedy.net.accounts.models.Entity
+    :type message_list: [speedy.core.im.models.Message]
+    :type entity: speedy.core.accounts.models.Entity
     """
     chats = set(message.chat for message in message_list)
     rmarks = {rmark.chat_id: rmark for rmark in ReadMark.objects.filter(chat__in=chats, entity=entity)}
@@ -53,8 +53,8 @@ def annotate_messages_with_read_marks(message_list, entity):
 @register.filter
 def get_chat_slug(chat, current_user):
     """
-    :type chat: speedy.net.im.models.Chat
-    :type current_user: speedy.net.accounts.models.Entity
+    :type chat: speedy.core.im.models.Chat
+    :type current_user: speedy.core.accounts.models.Entity
     :return: str
     """
     return chat.get_slug(current_user=current_user)
