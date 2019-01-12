29094d6d376b569656197bbf67de8cf0f8937fb9
Uri <uri@speedy.net>
2019-01-12 17:51:08 +0200

    - Removed django-debug-toolbar from requirements files and from all urls.

--- speedy/net/urls.py
+++ speedy/net/urls.py
@@ -25,10 +25,3 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + urlpatterns
 
-try:
-    import debug_toolbar
-    urlpatterns += [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    ]
-except ImportError:
-    pass
