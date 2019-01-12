e3e28362d461b14e014fad3d2cca2663076c52b7
Uri <uri@speedy.net>
2019-01-12 23:29:33 +0200

    Added tests_manage.py files - like manage.py but for tests.

---
+++ speedy/match/tests_manage.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import os
+import sys
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).absolute().parent.parent.parent))
+
+from speedy.core.settings.utils import env
+
+if (__name__ == "__main__"):
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "speedy.match.settings.{}".format(env('TESTS_ENVIRONMENT')))
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
+
+
