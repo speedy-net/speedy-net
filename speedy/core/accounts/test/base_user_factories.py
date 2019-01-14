a5e88c24dda901a4fdde966f10d73d6f45605a5e
Uri <uri@speedy.net>
2019-01-14 08:21:21 +0200

    Use method assert_user_first_and_last_name_in_all_languages in class DefaultUserFactory.

--- speedy/core/accounts/test/base_user_factories.py
+++ speedy/core/accounts/test/base_user_factories.py
@@ -8,12 +8,17 @@ from django.conf import settings as django_settings
 from django.test import TestCase as DjangoTestCase
 
 from speedy.core.base.test import tests_settings
+from speedy.core.accounts.test.mixins import SpeedyCoreAccountsModelsMixin
 from speedy.core.base.utils import normalize_username
-from speedy.core.accounts.models import User, UserEmailAddress
+from speedy.core.accounts.models import User
 
 
 if (django_settings.LOGIN_ENABLED):
-    _test_case = DjangoTestCase()
+    class DjangoTestCaseWithMixin(SpeedyCoreAccountsModelsMixin, DjangoTestCase):
+        pass
+
+
+    _test_case_with_mixin = DjangoTestCaseWithMixin()
 
 
     class DefaultUserFactory(factory.DjangoModelFactory):
@@ -31,24 +36,6 @@ if (django_settings.LOGIN_ENABLED):
 
         @factory.post_generation
         def validate_first_and_last_name_in_all_languages(self, created, extracted, **kwargs):
-            _test_case.assertTupleEqual(tuple1=User.LOCALIZABLE_FIELDS, tuple2=('first_name', 'last_name'))
-            _test_case.assertEqual(first=self.first_name_en, second=self.first_name)
-            _test_case.assertEqual(first=self.first_name_he, second=self.first_name)
-            _test_case.assertEqual(first=self.last_name_en, second=self.last_name)
-            _test_case.assertEqual(first=self.last_name_he, second=self.last_name)
-            field_name_localized_list = list()
-            for base_field_name in User.LOCALIZABLE_FIELDS:
-                for language_code in django_settings.ALL_LANGUAGE_CODES:
-                    field_name_localized = '{}_{}'.format(base_field_name, language_code)
-                    _test_case.assertEqual(first=getattr(self, field_name_localized), second=getattr(self, base_field_name), msg="DefaultUserFactory::fields don't match ({field_name_localized}, {base_field_name}), self.pk={self_pk}, self.username={self_username}, self.slug={self_slug}, self.profile.get_name()={self_profile_get_name}".format(
-                        field_name_localized=field_name_localized,
-                        base_field_name=base_field_name,
-                        self_pk=self.pk,
-                        self_username=self.username,
-                        self_slug=self.slug,
-                        self_profile_get_name=self.profile.get_name(),
-                    ))
-                    field_name_localized_list.append(field_name_localized)
-            _test_case.assertListEqual(list1=field_name_localized_list, list2=['first_name_en', 'first_name_he', 'last_name_en', 'last_name_he'])
+            _test_case_with_mixin.assert_user_first_and_last_name_in_all_languages(user=self)
 
 
