edcfedc1907f720afbefb78e408946d65074a53e
Uri <uri@speedy.net>
2019-01-16 18:19:55 +0200

    Updated admin.

--- speedy/core/accounts/admin.py
+++ speedy/core/accounts/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 from .models import User, Entity, UserEmailAddress
 
-admin.site.register(User)
-admin.site.register(Entity)
-admin.site.register(UserEmailAddress)
+
+class EntityAdmin(admin.ModelAdmin):
+    fields = ('date_created', 'date_updated', 'id', 'username', 'slug', 'photo')
+    readonly_fields = ('date_created', 'date_updated', 'id', 'username', 'slug', 'photo')
+
+
+class UserAdmin(admin.ModelAdmin):
+    # fields = ('date_created', 'date_updated')
+    readonly_fields = ('date_created', 'date_updated', 'id', 'first_name', 'last_name')
+
+
+class UserEmailAddressAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Entity, EntityAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(UserEmailAddress, UserEmailAddressAdmin)
 
 
