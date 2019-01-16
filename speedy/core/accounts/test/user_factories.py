6570e00edb9d58c8d855f8b7ea6a8129c774eb33
Uri <uri@speedy.net>
2019-01-16 14:18:24 +0200

    Use `django_settings.SITE_ID` instead of `Site.objects.get_current()`.

--- speedy/core/accounts/test/user_factories.py
+++ speedy/core/accounts/test/user_factories.py
@@ -1,12 +1,10 @@
 from django.conf import settings as django_settings
-from django.contrib.sites.models import Site
 
 
 if (django_settings.LOGIN_ENABLED):
-    site = Site.objects.get_current()
-    if (site.id == django_settings.SPEEDY_NET_SITE_ID):
+    if (django_settings.SITE_ID == django_settings.SPEEDY_NET_SITE_ID):
         from speedy.net.accounts.test.user_factories import DefaultUserFactory, InactiveUserFactory, ActiveUserFactory
-    if (site.id == django_settings.SPEEDY_MATCH_SITE_ID):
+    if (django_settings.SITE_ID == django_settings.SPEEDY_MATCH_SITE_ID):
         from speedy.match.accounts.test.user_factories import DefaultUserFactory, InactiveUserFactory, ActiveUserFactory
 
 
