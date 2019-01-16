50c08459fad8212c902aee91616f83175efb76b8
Uri <uri@speedy.net>
2019-01-16 17:39:56 +0200

    Fixed import.

--- speedy/core/im/models.py
+++ speedy/core/im/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.dispatch import receiver
 from django.utils.translation import gettext_lazy as _
 
-from speedy.core.base.models import TimeStampedModel, BaseManager, RegularUDIDField
+from speedy.core.base.models import TimeStampedModel, RegularUDIDField
 from speedy.core.accounts.models import Entity, User
 from .managers import ChatManager, MessageManager, ReadMarkManager
 
