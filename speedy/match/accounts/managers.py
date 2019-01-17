52a511f2b87e1fc58205b284c0334f18894793a3
Uri <uri@speedy.net>
2019-01-17 08:08:21 +0200

    Trying to improve performance.

--- speedy/match/accounts/managers.py
+++ speedy/match/accounts/managers.py
@@ -1,13 +1,15 @@
+from django.utils.translation import get_language
+
 from speedy.core.accounts.models import User
 from speedy.core.base.utils import get_age_ranges_match
-
 from speedy.core.base.models import BaseManager
 
 
 class SiteProfileManager(BaseManager):
     def get_matches(self, user_profile):
         age_ranges = get_age_ranges_match(min_age=user_profile.min_age_match, max_age=user_profile.max_age_match)
-        qs = User.objects.active(gender__in=user_profile.gender_to_match, date_of_birth__range=age_ranges).exclude(pk=user_profile.user_id).distinct()
+        language_code = get_language()
+        qs = User.objects.active(gender__in=user_profile.gender_to_match, date_of_birth__range=age_ranges, speedy_match_site_profile__active_languages__contains=language_code).exclude(pk=user_profile.user_id).distinct()
 
         # user.speedy_match_profile.is_active may be changed by user_profile.get_matching_rank() so we call it twice to improve performance.
         qs = [user for user in qs if ((user.speedy_match_profile.is_active) and (user_profile.get_matching_rank(other_profile=user.speedy_match_profile) > self.model.RANK_0) and (user.speedy_match_profile.is_active))]
