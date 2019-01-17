50b51d8abbb597d4df42744c9663ce204626e1bc
Uri <uri@speedy.net>
2019-01-17 07:46:05 +0200

    # user.speedy_match_profile.is_active may be changed by user_profile.get_matching_rank() so we call it twice to improve performance.

--- speedy/match/accounts/managers.py
+++ speedy/match/accounts/managers.py
@@ -9,7 +9,8 @@ class SiteProfileManager(BaseManager):
         age_ranges = get_age_ranges_match(min_age=user_profile.min_age_match, max_age=user_profile.max_age_match)
         qs = User.objects.active(gender__in=user_profile.gender_to_match, date_of_birth__range=age_ranges).exclude(pk=user_profile.user_id).distinct()
 
-        qs = [user for user in qs if ((user_profile.get_matching_rank(other_profile=user.speedy_match_profile) > self.model.RANK_0) and (user.speedy_match_profile.is_active))]
+        # user.speedy_match_profile.is_active may be changed by user_profile.get_matching_rank() so we call it twice to improve performance.
+        qs = [user for user in qs if ((user.speedy_match_profile.is_active) and (user_profile.get_matching_rank(other_profile=user.speedy_match_profile) > self.model.RANK_0) and (user.speedy_match_profile.is_active))]
 
         qs = sorted(qs, key=lambda user: (user.speedy_match_profile.rank, user.speedy_match_profile.last_visit), reverse=True)
         return qs
