8d66b54ab4efd6c995f45956e4b989cc9b100e93
Uri <uri@speedy.net>
2019-07-11 09:03:41 +0300

    Updated translations.

--- speedy/core/__translations/__translations.py
+++ speedy/core/__translations/__translations.py
@@ -86,6 +86,50 @@ pgettext_lazy(context="female", message='Carnist'),
 pgettext_lazy(context="male", message='Carnist'),
 pgettext_lazy(context="other", message='Carnist'),
 
+pgettext_lazy(context="female", message="No")
+pgettext_lazy(context="male", message="No")
+pgettext_lazy(context="other", message="No")
+
+pgettext_lazy(context="female", message="Sometimes")
+pgettext_lazy(context="male", message="Sometimes")
+pgettext_lazy(context="other", message="Sometimes")
+
+pgettext_lazy(context="female", message="Yes")
+pgettext_lazy(context="male", message="Yes")
+pgettext_lazy(context="other", message="Yes")
+
+pgettext_lazy(context="female", message="Single")
+pgettext_lazy(context="male", message="Single")
+pgettext_lazy(context="other", message="Single")
+
+pgettext_lazy(context="female", message="Divorced")
+pgettext_lazy(context="male", message="Divorced")
+pgettext_lazy(context="other", message="Divorced")
+
+pgettext_lazy(context="female", message="Widowed")
+pgettext_lazy(context="male", message="Widowed")
+pgettext_lazy(context="other", message="Widowed")
+
+pgettext_lazy(context="female", message="In a relationship")
+pgettext_lazy(context="male", message="In a relationship")
+pgettext_lazy(context="other", message="In a relationship")
+
+pgettext_lazy(context="female", message="In an open relationship")
+pgettext_lazy(context="male", message="In an open relationship")
+pgettext_lazy(context="other", message="In an open relationship")
+
+pgettext_lazy(context="female", message="It's complicated")
+pgettext_lazy(context="male", message="It's complicated")
+pgettext_lazy(context="other", message="It's complicated")
+
+pgettext_lazy(context="female", message="Separated")
+pgettext_lazy(context="male", message="Separated")
+pgettext_lazy(context="other", message="Separated")
+
+pgettext_lazy(context="female", message="Married")
+pgettext_lazy(context="male", message="Married")
+pgettext_lazy(context="other", message="Married")
+
 pgettext_lazy(context="female", message='Your new password has been saved.')
 pgettext_lazy(context="male", message='Your new password has been saved.')
 pgettext_lazy(context="other", message='Your new password has been saved.')
