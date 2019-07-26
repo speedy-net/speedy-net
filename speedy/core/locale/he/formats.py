675c4a1814a9ebcd118d800e8e0e30512e1a3c1f
Uri <uri@speedy.net>
2019-07-26 07:58:52 +0300

    Added locale formats.

---
+++ speedy/core/locale/he/formats.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+
+DATE_FORMAT = 'j בF Y'
+MONTH_DAY_FORMAT = 'j בF'
+YEAR_FORMAT = 'Y'
+
+
+DATE_INPUT_FORMATS = [
+    '%Y-%m-%d',                           # '2006-10-25',
+]
+
+
