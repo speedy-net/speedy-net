054c55bcdb9f338e91c21045b6f25e14020c56a6
Uri <uri@speedy.net>
2019-08-01 08:28:14 +0300

    Updated models.

--- speedy/core/friends/models.py
+++ speedy/core/friends/models.py
@@ -4,10 +4,11 @@ from django.dispatch import receiver
 from friendship.models import Friend, FriendshipRequest
 
 from speedy.core.blocks.models import Block
+from speedy.match.likes.models import UserLike
 
 
 @receiver(signal=models.signals.post_save, sender=Block)
-def delete_friendship_requests_on_block(sender, instance: Block, **kwargs):
+def cancel_friendship_requests_on_block(sender, instance: Block, **kwargs):
     q1 = Q(from_user=instance.blocker, to_user=instance.blocked)
     q2 = Q(from_user=instance.blocked, to_user=instance.blocker)
     active_requests = FriendshipRequest.objects.filter(q1 | q2)
@@ -16,5 +17,12 @@ def delete_friendship_requests_on_block(sender, instance: Block, **kwargs):
 
 
 @receiver(signal=models.signals.post_save, sender=Block)
-def delete_friends_on_block(sender, instance: Block, **kwargs):
+def remove_friends_on_block(sender, instance: Block, **kwargs):
     Friend.objects.remove_friend(from_user=instance.blocker, to_user=instance.blocked)
+
+
+@receiver(signal=models.signals.post_save, sender=Block)
+def remove_like_on_block(sender, instance: Block, **kwargs):
+    UserLike.objects.remove_like(from_user=instance.blocker, to_user=instance.blocked)
+
+
