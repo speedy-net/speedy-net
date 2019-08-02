e0f78ae9fba58f59728bdf2dd9f77f1a59cd630b
Uri <uri@speedy.net>
2019-08-02 07:07:59 +0300

    Design

--- speedy/core/accounts/views.py
+++ speedy/core/accounts/views.py
@@ -63,7 +63,7 @@ class IndexView(generic.View):
 
 
 class RegistrationView(FormValidMessageMixin, generic.CreateView):
-    template_name = 'accounts/registration.html'
+    template_name = 'accounts/registration/registration.html'
     form_class = RegistrationForm
     form_valid_message = _("Registration complete. Don't forget to confirm your email.")
 
