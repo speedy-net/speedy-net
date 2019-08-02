0e91ac51f767a71675c2ca59e3a2af28742c66b4
Uri <uri@speedy.net>
2019-08-02 09:22:05 +0300

    Likes - changed to paginate_by = 24 (should be divisible by 2, 3 and 4).

--- speedy/match/likes/views.py
+++ speedy/match/likes/views.py
@@ -18,7 +18,7 @@ class LikeListDefaultRedirectView(UserMixin, generic.RedirectView):
 class LikeListViewBase(UserMixin, PermissionRequiredMixin, generic.ListView):
     permission_required = 'likes.view_likes'
     template_name = 'likes/like_list.html'
-    paginate_by = 25
+    paginate_by = 24
 
     def get_context_data(self, **kwargs):
         cd = super().get_context_data(**kwargs)
