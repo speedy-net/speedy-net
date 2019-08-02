eca1e18a5b7b87686d3d5eb06832ffc4acf3ea6c
Uri <uri@speedy.net>
2019-08-02 10:08:34 +0300

    tmp

--- speedy/match/matches/views.py
+++ speedy/match/matches/views.py
@@ -24,13 +24,13 @@ class MatchesListView(LoginRequiredMixin, generic.UpdateView):
         matches_list = SpeedyMatchSiteProfile.objects.get_matches(self.request.user.speedy_match_profile)
         page = self.request.GET.get('page', 1)
 
-        paginator = Paginator(matches_list, self.paginate_by)
+        self.paginator = Paginator(matches_list, self.paginate_by)
         try:
-            matches_list = paginator.page(page)
+            matches_list = self.paginator.page(page)
         except PageNotAnInteger:
-            matches_list = paginator.page(1)
+            matches_list = self.paginator.page(1)
         except EmptyPage:
-            matches_list = paginator.page(paginator.num_pages)
+            matches_list = self.paginator.page(self.paginator.num_pages)
         return matches_list
         # return SpeedyMatchSiteProfile.objects.get_matches(self.request.user.speedy_match_profile)[:60] # ~~~~ TODO: We need pagination.
         # return SpeedyMatchSiteProfile.objects.get_matches(self.request.user.speedy_match_profile)[:3] # ~~~~ TODO: We need pagination.
@@ -40,8 +40,10 @@ class MatchesListView(LoginRequiredMixin, generic.UpdateView):
 
     def get_context_data(self, **kwargs):
         cd = super().get_context_data(**kwargs)
+        matches_list = self.get_matches()
         cd.update({
-            'matches': self.get_matches(),
+            'matches_list': matches_list,
+            'paginator': self.paginator,
         })
         return cd
 
