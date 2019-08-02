1b2f25333fa7ee8dad96d81287d57494a41483af
Uri <uri@speedy.net>
2019-08-02 15:07:06 +0300

    tmp

--- speedy/match/accounts/managers.py
+++ speedy/match/accounts/managers.py
@@ -9,7 +9,7 @@ class SiteProfileManager(BaseManager):
     def get_matches(self, user_profile):
         age_ranges = get_age_ranges_match(min_age=user_profile.min_age_match, max_age=user_profile.max_age_match)
         language_code = get_language()
-        qs = User.objects.active(gender__in=user_profile.gender_to_match, date_of_birth__range=age_ranges, speedy_match_site_profile__active_languages__contains=language_code).exclude(pk=user_profile.user_id).select_related(self.model.RELATED_NAME).distinct()
+        qs = User.objects.active(gender__in=user_profile.gender_to_match, date_of_birth__range=age_ranges, speedy_match_site_profile__active_languages__contains=language_code).exclude(pk=user_profile.user_id).prefetch_related(self.model.RELATED_NAME).distinct()
         # qs = list(qs) # ~~~~ TODO: remove this line!
         # for i in range(10): # ~~~~ TODO: remove this line!
         #     qs = qs + qs # ~~~~ TODO: remove this line!
