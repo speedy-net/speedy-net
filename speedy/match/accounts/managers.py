2059777439856a80347c2436ba474bff7c747f6e
Uri <uri@speedy.net>
2019-08-03 07:16:32 +0300

    Optimized search.

--- speedy/match/accounts/managers.py
+++ speedy/match/accounts/managers.py
@@ -10,7 +10,13 @@ class SiteProfileManager(BaseManager):
         age_ranges = get_age_ranges_match(min_age=user_profile.min_age_match, max_age=user_profile.max_age_match)
         language_code = get_language()
         qs = User.objects.active(gender__in=user_profile.gender_to_match, date_of_birth__range=age_ranges, speedy_match_site_profile__active_languages__contains=language_code).exclude(pk=user_profile.user_id).prefetch_related(self.model.RELATED_NAME).distinct().order_by('-speedy_match_site_profile__last_visit')
-        matches_list = [user for user in qs if ((user.speedy_match_profile.is_active) and (user_profile.get_matching_rank(other_profile=user.speedy_match_profile) > self.model.RANK_0))]
+        user_list = []
+        matches_list = []
+        for user in qs:
+            if ((len(matches_list) < 360) or (len(user_list) < 600)):
+                user_list.append(user)
+                if ((user.speedy_match_profile.is_active) and (user_profile.get_matching_rank(other_profile=user.speedy_match_profile) > self.model.RANK_0)):
+                    matches_list.append(user)
         matches_list = sorted(matches_list, key=lambda user: (user.speedy_match_profile.rank, user.speedy_match_profile.last_visit), reverse=True)
         return matches_list
 
