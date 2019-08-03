b849ee4fe4cf3a25f450bd66c53478db7d510191
Uri <uri@speedy.net>
2019-08-03 06:52:42 +0300

    Improved speed.

--- speedy/match/accounts/managers.py
+++ speedy/match/accounts/managers.py
@@ -9,8 +9,12 @@ class SiteProfileManager(BaseManager):
     def get_matches(self, user_profile):
         age_ranges = get_age_ranges_match(min_age=user_profile.min_age_match, max_age=user_profile.max_age_match)
         language_code = get_language()
-        qs = User.objects.active(gender__in=user_profile.gender_to_match, date_of_birth__range=age_ranges, speedy_match_site_profile__active_languages__contains=language_code).exclude(pk=user_profile.user_id).prefetch_related(self.model.RELATED_NAME).distinct()
-        matches_list = [user for user in qs if ((user.speedy_match_profile.is_active) and (user_profile.get_matching_rank(other_profile=user.speedy_match_profile) > self.model.RANK_0))]
+        qs = User.objects.active(gender__in=user_profile.gender_to_match, date_of_birth__range=age_ranges, speedy_match_site_profile__active_languages__contains=language_code).exclude(pk=user_profile.user_id).prefetch_related(self.model.RELATED_NAME).distinct().order_by('-speedy_match_site_profile__last_visit')
+        matches_list = []
+        for slice in [600, 6000, None]:
+            if (len(matches_list) < 360):
+                user_list = qs[:slice]
+                matches_list = [user for user in user_list if ((user.speedy_match_profile.is_active) and (user_profile.get_matching_rank(other_profile=user.speedy_match_profile) > self.model.RANK_0))]
         matches_list = sorted(matches_list, key=lambda user: (user.speedy_match_profile.rank, user.speedy_match_profile.last_visit), reverse=True)
         return matches_list
 
