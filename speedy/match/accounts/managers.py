5bd378a65c8e2ee7a5e78f9f3b951ad0cfc2feb1
Uri <uri@speedy.net>
2019-08-03 11:52:12 +0300

    Optimized search again.

--- speedy/match/accounts/managers.py
+++ speedy/match/accounts/managers.py
@@ -12,25 +12,14 @@ class SiteProfileManager(BaseManager):
         qs = User.objects.active(gender__in=user_profile.gender_to_match, date_of_birth__range=age_ranges, speedy_match_site_profile__active_languages__contains=language_code).exclude(pk=user_profile.user_id).prefetch_related(self.model.RELATED_NAME).distinct().order_by('-speedy_match_site_profile__last_visit')
         u1_list = [user for user in qs if ((user.speedy_match_profile.is_active) and (user_profile._1___get_matching_rank_quick_without_testing(other_profile=user.speedy_match_profile) > self.model.RANK_0))]
         u1_list = sorted(u1_list, key=lambda user: (user.speedy_match_profile._1___rank, user.speedy_match_profile.last_visit), reverse=True)
-        user_list = []
         matches_list = []
-        rank_list = {
-            self.model.RANK_1: [],
-            self.model.RANK_2: [],
-            self.model.RANK_3: [],
-            self.model.RANK_4: [],
-            self.model.RANK_5: [],
-        }
         for user in u1_list:
-            if ((len(matches_list) < 360) or (len(user_list) < 600)):
-                user_list.append(user)
-                if (offset - 75 < len(matches_list) < offset + 50):
-                    user.speedy_match_profile.rank = user_profile.get_matching_rank(other_profile=user.speedy_match_profile)
-                else:
-                    user.speedy_match_profile.rank = user.speedy_match_profile._1___rank#####
-                if ((user.speedy_match_profile.is_active) and (user.speedy_match_profile.rank > self.model.RANK_0)):
-                    matches_list.append(user)
-                    rank_list[user.speedy_match_profile.rank].append(user)
+            if (offset - 75 < len(matches_list) < offset + 50):
+                user.speedy_match_profile.rank = user_profile.get_matching_rank(other_profile=user.speedy_match_profile)
+            else:
+                user.speedy_match_profile.rank = user.speedy_match_profile._1___rank#####
+            if ((user.speedy_match_profile.is_active) and (user.speedy_match_profile.rank > self.model.RANK_0)):
+                matches_list.append(user)
         matches_list = sorted(matches_list, key=lambda user: (user.speedy_match_profile.rank, user.speedy_match_profile.last_visit), reverse=True)
         return matches_list
 
