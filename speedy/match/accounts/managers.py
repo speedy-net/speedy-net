1d15d78c2d9bd00dd2745e1084f8d41cba0c9c0b
Uri <uri@speedy.net>
2019-08-03 10:09:26 +0300

    Optimized search.

--- speedy/match/accounts/managers.py
+++ speedy/match/accounts/managers.py
@@ -12,11 +12,19 @@ class SiteProfileManager(BaseManager):
         qs = User.objects.active(gender__in=user_profile.gender_to_match, date_of_birth__range=age_ranges, speedy_match_site_profile__active_languages__contains=language_code).exclude(pk=user_profile.user_id).prefetch_related(self.model.RELATED_NAME).distinct().order_by('-speedy_match_site_profile__last_visit')
         user_list = []
         matches_list = []
+        rank_list = {
+            self.model.RANK_1: [],
+            self.model.RANK_2: [],
+            self.model.RANK_3: [],
+            self.model.RANK_4: [],
+            self.model.RANK_5: [],
+        }
         for user in qs:
-            if ((len(matches_list) < 360) or (len(user_list) < 600)):
+            if ((len(rank_list[self.model.RANK_5]) < 240) or (len(matches_list) < 360) or (len(user_list) < 600)):
                 user_list.append(user)
                 if ((user.speedy_match_profile.is_active) and (user_profile.get_matching_rank(other_profile=user.speedy_match_profile) > self.model.RANK_0)):
                     matches_list.append(user)
+                    rank_list[user.speedy_match_profile.rank].append(user)
         matches_list = sorted(matches_list, key=lambda user: (user.speedy_match_profile.rank, user.speedy_match_profile.last_visit), reverse=True)
         return matches_list
 
