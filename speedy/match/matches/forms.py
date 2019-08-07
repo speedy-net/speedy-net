3d4464613767ed1213a0818ad86b6a36e4cdc732
Uri <uri@speedy.net>
2019-08-07 18:45:33 +0300

    crispy commit.

--- speedy/match/matches/forms.py
+++ speedy/match/matches/forms.py
@@ -18,16 +18,16 @@ class SpeedyMatchProfileFullSettingsBaseForm(SpeedyMatchProfileBaseForm):
         super().__init__(*args, **kwargs)
         self.helper = FormHelperWithDefaults()
         # split into two columns
-        field_names = list(self.fields.keys())
-        custom_field_names = ('gender_to_match', 'diet_match', 'smoking_status_match', 'relationship_status_match')
-        self.helper.add_layout(Div(*[
-            Row(*[
-                # a little hack that forces display of custom widgets
-                Div(Field(field, template='%s/speedy_match_custom_field.html') if (field in custom_field_names) else field, css_class='col-md-6')
-                for field in pair
-            ])
-            for pair in zip_longest(field_names[::2], field_names[1::2])
-        ]))
+        # field_names = list(self.fields.keys())
+        # custom_field_names = ('gender_to_match', 'diet_match', 'smoking_status_match', 'relationship_status_match')
+        # self.helper.add_layout(Div(*[
+        #     Row(*[
+        #         # a little hack that forces display of custom widgets
+        #         Div(Field(field, template='%s/speedy_match_custom_field.html') if (field in custom_field_names) else field, css_class='col-md-6')
+        #         for field in pair
+        #     ])
+        #     for pair in zip_longest(field_names[::2], field_names[1::2])
+        # ]))
         self.helper.add_input(Submit('submit', pgettext_lazy(context=self.instance.user.get_gender(), message='Save Changes')))
 
 
