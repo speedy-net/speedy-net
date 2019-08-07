d5ed8d75c6182388339d8677383291ac89dce499
Uri <uri@speedy.net>
2019-08-07 10:28:46 +0300

    Updated management command.

--- speedy/net/accounts/management/commands/delete_unconfirmed_accounts.py
+++ speedy/net/accounts/management/commands/delete_unconfirmed_accounts.py
@@ -3,15 +3,14 @@ from datetime import timedelta
 from django.core.management import BaseCommand
 from django.utils.timezone import now
 
-from speedy.core.accounts.models import UserEmailAddress
+from speedy.core.accounts.models import User
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        emails = UserEmailAddress.objects.filter(is_confirmed=False, date_created__lte=(now() - timedelta(days=10))).exclude(user__is_staff=True)
+        users = User.objects.filter(date_created__lte=(now() - timedelta(days=10))).exclude(is_staff=True)
 
-        for email in emails:
-            user = email.user
+        for user in users:
             if (not (user.is_staff)):
                 if (not (user.has_confirmed_email())):
                     user.delete()
