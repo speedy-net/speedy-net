df998c15117201ee92021e033eb6cb900613ccda
Uri <uri@speedy.net>
2019-08-10 05:29:56 +0300

    Management commands.

--- speedy/net/accounts/management/commands/delete_unconfirmed_accounts.py
+++ speedy/net/accounts/management/commands/delete_unconfirmed_accounts.py
@@ -4,19 +4,30 @@ from datetime import timedelta
 from django.core.management import BaseCommand
 from django.utils.timezone import now
 
-from speedy.core.accounts.models import User
+from speedy.core.accounts.models import User, UserEmailAddress
 
 logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        emails = UserEmailAddress.objects.filter(is_confirmed=False, date_created__lte=(now() - timedelta(days=10)), confirmation_sent__gte=2).exclude(user__is_staff=True)
+
+        for email in emails:
+            user = email.user
+            if (not (user.is_staff)):
+                if (email.date_created <= now() - timedelta(days=10)):
+                    if ((email.is_confirmed is False) and (email.confirmation_sent >= 2)):
+                        logger.warning("Deleting email {} of user {} - unconfirmed. Confirmation sent {} times, Added on {}.".format(email, user, email.confirmation_sent, email.date_created))
+                        email.delete()
+
         users = User.objects.filter(date_created__lte=(now() - timedelta(days=10))).exclude(is_staff=True)
 
         for user in users:
             if (not (user.is_staff)):
-                if (not (user.has_confirmed_email())):
-                    logger.warning("Deleting user {} - no confirmed email. Registered on {}.".format(user, user.date_created))
-                    user.delete()
+                if (user.date_created <= now() - timedelta(days=10)):
+                    if (not (user.has_confirmed_email())):
+                        logger.warning("Deleting user {} - no confirmed email. Registered on {}.".format(user, user.date_created))
+                        user.delete()
 
 
--- speedy/net/accounts/management/commands/send_confirmation_reminders.py
+++ speedy/net/accounts/management/commands/send_confirmation_reminders.py
@@ -1,3 +1,4 @@
+import logging
 from datetime import timedelta
 
 from django.core.management import BaseCommand
@@ -5,12 +6,18 @@ from django.utils.timezone import now
 
 from speedy.core.accounts.models import UserEmailAddress
 
+logger = logging.getLogger(__name__)
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         emails = UserEmailAddress.objects.filter(is_confirmed=False, date_created__lte=(now() - timedelta(days=5)), confirmation_sent__lte=1).exclude(confirmation_token='')
         for email in emails:
-            if (email.user.speedy_net_profile.is_active):
-                email.send_confirmation_email()
+            user = email.user
+            if (email.date_created <= now() - timedelta(days=5)):
+                if ((email.is_confirmed is False) and (email.confirmation_sent <= 1)):
+                    if (user.is_active):
+                        logger.debug("Sending confirmation to email {} of user {}. Confirmation sent {} times, Added on {}.".format(email, user, email.confirmation_sent, email.date_created))
+                        email.send_confirmation_email()
 
 
