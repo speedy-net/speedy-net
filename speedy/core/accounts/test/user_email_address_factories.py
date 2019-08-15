2f1eaf2b51b981a0f9bde1379dcbf2dda5aa61d8
Uri <uri@speedy.net>
2019-08-15 16:53:46 +0300

    Fixed tests bug - email addresses in tests will be random.

--- speedy/core/accounts/test/user_email_address_factories.py
+++ speedy/core/accounts/test/user_email_address_factories.py
@@ -3,8 +3,8 @@ import factory.fuzzy
 
 from django.conf import settings as django_settings
 
+from speedy.core.base.utils import generate_regular_udid as generate_random_id
 from speedy.core.accounts.models import UserEmailAddress
-
 
 if (django_settings.LOGIN_ENABLED):
     from speedy.core.accounts.test.base_user_factories import DefaultUserFactory
@@ -12,7 +12,7 @@ if (django_settings.LOGIN_ENABLED):
 
     class UserEmailAddressFactory(factory.DjangoModelFactory):
         user = factory.SubFactory(DefaultUserFactory)
-        email = factory.Faker('email')
+        email = factory.LazyAttribute(lambda o: '{id}@example.speedy.net'.format(id=generate_random_id()).lower())
 
         class Meta:
             model = UserEmailAddress
