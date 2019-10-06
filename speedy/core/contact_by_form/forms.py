1dbf130e07789e9608196898102aad32baaac927
Uri <uri@speedy.net>
2019-10-06 19:37:15 +0300

    Added _not_allowed_strings to FeedbackForm.

--- speedy/core/contact_by_form/forms.py
+++ speedy/core/contact_by_form/forms.py
@@ -1,11 +1,14 @@
 from crispy_forms.layout import Submit, Div, Row
 from django.utils.translation import gettext_lazy as _, pgettext_lazy
+from django.core.exceptions import ValidationError
 
 from speedy.core.base.forms import ModelFormWithDefaults, FormHelperWithDefaults
 from .models import Feedback
 
 
 class FeedbackForm(ModelFormWithDefaults):
+    _not_allowed_strings = ["monkeydigital.co@gmail.com", "https://monkeydigital.co/"]
+
     class Meta:
         model = Feedback
         fields = ('sender_name', 'sender_email', 'text')
@@ -25,5 +28,12 @@ class FeedbackForm(ModelFormWithDefaults):
                 Div('sender_email', css_class='col-md-6'),
             ))
             self.helper.add_input(Submit('submit', _('Send')))
+
+    def clean_text(self):
+        text = self.cleaned_data.get('text')
+        for not_allowed_string in self._not_allowed_strings:
+            if (not_allowed_string in text):
+                raise ValidationError(_("Please contact us by e-mail."))
+        return text
 
 
