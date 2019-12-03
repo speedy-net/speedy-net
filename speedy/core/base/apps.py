c1549acf17fa3d1742563e2c2248682ff43fb502
Uri <uri@speedy.net>
2019-12-03 06:23:51 +0200

    Patched Django's FORMAT_SETTINGS to add YEAR_FORMAT.

--- speedy/core/base/apps.py
+++ speedy/core/base/apps.py
@@ -1,10 +1,15 @@
 from django.utils.translation import gettext_lazy as _
 from django.apps import AppConfig
 
+from speedy.core.patches import locale_patches
+
 
 class SpeedyCoreBaseConfig(AppConfig):
     name = 'speedy.core.base'
     verbose_name = _("Speedy Core Base App")
     label = 'base'
+
+    def ready(self):
+        locale_patches.patch()
 
 
