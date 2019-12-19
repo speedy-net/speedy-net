584a408126e45ceb77607c3cbb2a87092078cf32
Uri <uri@speedy.net>
2019-12-19 16:27:30 +0200

    Updated comment.

--- speedy/core/friends/widgets.py
+++ speedy/core/friends/widgets.py
@@ -7,8 +7,10 @@ class UserFriendsWidget(Widget):
     template_name = 'friends/user_friends_widget.html'
 
     def get_random_friends(self, count):
-        # Select count random friends from the list.
-        # If there are less than count friends, return all of them in random order.
+        """
+        Select <count> random friends from the list of user's friends, without repetition.
+        If there are less than <count> friends, return all of them in random order.
+        """
         user_friends = self.user.all_friends
         friends_to_return = min(len(user_friends), count)
         random_friends = random.sample(user_friends, friends_to_return)
