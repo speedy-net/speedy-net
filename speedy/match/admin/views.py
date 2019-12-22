1734ae312176f4126fe8e2272d780d79fc29de46
Uri <uri@speedy.net>
2019-12-22 05:44:02 +0200

    Updated admin.

--- speedy/match/admin/views.py
+++ speedy/match/admin/views.py
@@ -1,4 +1,7 @@
-from django.utils.translation import get_language
+from datetime import timedelta
+
+from django.utils.translation import get_language, gettext_lazy as _
+from django.utils.timezone import now
 from django.views import generic
 
 from speedy.core.admin.mixins import OnlyAdminMixin
@@ -13,6 +16,31 @@ class AdminMatchesListView(OnlyAdminMixin, generic.ListView):
     page_size = 96
     paginate_by = page_size
 
+    def get_total_number_of_active_members_text(self):
+        language_code = get_language()
+        total_number_of_active_members = User.objects.active(
+            speedy_match_site_profile__active_languages__contains=[language_code],
+        ).count()
+        total_number_of_active_members_in_the_last_week = User.objects.active(
+            speedy_match_site_profile__active_languages__contains=[language_code],
+            speedy_match_site_profile__last_visit__gte=now() - timedelta(days=7),
+        ).count()
+        total_number_of_active_members_in_the_last_month = User.objects.active(
+            speedy_match_site_profile__active_languages__contains=[language_code],
+            speedy_match_site_profile__last_visit__gte=now() - timedelta(days=30),
+        ).count()
+        total_number_of_active_members_in_the_last_four_months = User.objects.active(
+            speedy_match_site_profile__active_languages__contains=[language_code],
+            speedy_match_site_profile__last_visit__gte=now() - timedelta(days=120),
+        ).count()
+        total_number_of_active_members_text = _("Admin: The total number of active members on the site is {total_number_of_active_members}, of which {total_number_of_active_members_in_the_last_week} members entered the site in the last week, {total_number_of_active_members_in_the_last_month} members entered the site in the last month, and {total_number_of_active_members_in_the_last_four_months} members entered the site in the last four months.").format(
+            total_number_of_active_members=total_number_of_active_members,
+            total_number_of_active_members_in_the_last_week=total_number_of_active_members_in_the_last_week,
+            total_number_of_active_members_in_the_last_month=total_number_of_active_members_in_the_last_month,
+            total_number_of_active_members_in_the_last_four_months=total_number_of_active_members_in_the_last_four_months,
+        )
+        return total_number_of_active_members_text
+
     def get_queryset(self):
         SiteProfile = get_site_profile_model()
         language_code = get_language()
@@ -25,6 +53,7 @@ class AdminMatchesListView(OnlyAdminMixin, generic.ListView):
         cd = super().get_context_data(**kwargs)
         cd.update({
             'matches_list': cd['object_list'],
+            'total_number_of_active_members_text': self.get_total_number_of_active_members_text(),
         })
         return cd
 
