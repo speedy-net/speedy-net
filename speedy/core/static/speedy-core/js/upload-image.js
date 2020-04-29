1542f165658c71e34af3595651a027040fa31731
Uri <uri@speedy.net>
2020-04-29 12:02:26 +0300

    Temp commit.

--- speedy/core/static/speedy-core/js/upload-image.js
+++ speedy/core/static/speedy-core/js/upload-image.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-    $('.user-photo-input-wrapper #id_photo').change(function () {
+    $('.user-photo-input-wrapper #id_photo').change(function() {
         var file = this.files[0];
         var reader = new FileReader();
         reader.onloadend = function () {
-           $('.image-click-zone').css('background-image', 'url("' + reader.result + '")').addClass('uploaded');
+            $('.image-click-zone').css('background-image', 'url("' + reader.result + '")').addClass('uploaded');
         };
         if (file) {
             reader.readAsDataURL(file);
@@ -11,8 +11,8 @@ $(document).ready(function() {
         }
     });
 
-    $('.image-click-zone').click(function(){
-        $(this).closest('.activation-form-field').find('#id_photo').click()
+    $('.image-click-zone').click(function() {
+        $(this).closest('.form-field').find('#id_photo').click()
     });
 });
 
