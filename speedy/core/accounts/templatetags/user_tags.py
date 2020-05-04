9320aa2fc813d8e682acc5f6468b1eb6a97e2eb8
Uri <uri@speedy.net>
2020-05-04 15:41:11 +0300

    Updated height in profile_picture tag.

--- speedy/core/accounts/templatetags/user_tags.py
+++ speedy/core/accounts/templatetags/user_tags.py
@@ -8,10 +8,17 @@ register = template.Library()
 @register.inclusion_tag(filename='accounts/profile_picture.html', takes_context=True)
 def profile_picture(context, user, geometry, with_link=True, html_class=''):
     context = copy.copy(context)
+    geometry_splitted = geometry.split('x')
+    width = geometry_splitted[0]
+    if (len(geometry_splitted) == 2):
+        height = geometry_splitted[1]
+    else:
+        height = geometry_splitted[0]
     context.update({
         'user': user,
         'geometry': geometry,
-        'width': geometry.split('x')[0],
+        'width': width,
+        'height': height,
         'with_link': with_link,
         'html_class': html_class,
     })
