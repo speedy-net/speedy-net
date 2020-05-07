8676577e28c7eb7c8179dc9d91ef37bc76bfc4f7
Uri <uri@speedy.net>
2020-05-07 14:05:55 +0300

    Round aspect_ratio to 3 digits (after the dot).

--- speedy/core/accounts/templatetags/user_tags.py
+++ speedy/core/accounts/templatetags/user_tags.py
@@ -14,12 +14,13 @@ def profile_picture(context, user, geometry, with_link=True, html_class=''):
         height = geometry_splitted[1]
     else:
         height = geometry_splitted[0]
+    aspect_ratio = round(float(height) / float(width) * 100, 3)
     context.update({
         'user': user,
         'geometry': geometry,
         'width': width,
         'height': height,
-        'aspect_ratio': float(height) / float(width) * 100,
+        'aspect_ratio': aspect_ratio,
         'with_link': with_link,
         'html_class': html_class,
     })
