049fc0ac3de11d19019112f214c1158b6b705fcd
Uri <uri@speedy.net>
2020-05-11 19:22:34 +0300

    - Updated translations. - Removed __eq__ and __hash__ from class Entity

--- speedy/core/accounts/models.py
+++ speedy/core/accounts/models.py
@@ -100,19 +100,6 @@ class Entity(CleanAndValidateAllFieldsMixin, TimeStampedModel):
         verbose_name_plural = _('entities')
         ordering = ('id',)
 
-    # def __eq__(self, other):
-    #     if (not (isinstance(other, Entity))):
-    #         return False
-    #     # Skip the following check from django.db.models.Model
-    #     # if self._meta.concrete_model != other._meta.concrete_model:
-    #     #     return False
-    #     if self.pk is None:
-    #         return self is other
-    #     return self.pk == other.pk
-    #
-    # def __hash__(self):
-    #     return super().__hash__()
-    #
     def __str__(self):
         return '<Entity {} - {}>'.format(self.id, self.slug)
         # return '<Entity {} - username={}, slug={}>'.format(self.id, self.username, self.slug)
