1dfd5295d7ffd2f4f1ff4602cadab49a3c2a1774
Uri <uri@speedy.net>
2020-08-23 13:19:25 +0300

    - Upgraded requirements. - Installed Django from fork (-e git://github.com/speedy-net/django.git@3.0.9a#egg=django). - Uninstall Django in deploy.sh before installing from fork.

--- contrib/deploy.sh
+++ contrib/deploy.sh
@@ -9,6 +9,7 @@ cd ${DIR}
 git pull
 ${PY} -m pip install --upgrade pip
 ${PIP} install --upgrade setuptools wheel
+${PIP} uninstall Django # installed from Speedy Net fork. # ~~~~ TODO: When we upgrade Django to 3.1.*, this will not be necessary.
 ${PIP} install --upgrade -r requirements.txt
 ${PIP} uninstall django-debug-toolbar # django-debug-toolbar should never be installed in production or staging.
 
