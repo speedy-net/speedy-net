9dd9e70491c12a2237518d72063937b0ad53a7fb
Aaron Chong <aaronjhchong@hotmail.com>
2020-09-03 02:11:01 +0800

    Patch must_update to allow Django 3.0.6 sessions
    Instead of forcibly staying on lower iterations.

--- speedy/core/patches/session_patches.py
+++ speedy/core/patches/session_patches.py
@@ -2,4 +2,11 @@ from django.contrib.auth.hashers import PBKDF2PasswordHasher
 
 
 def patch():
-    PBKDF2PasswordHasher.iterations = 180000  # Django 3.0.6
+    def must_update(self, encoded):
+        algorithm, iterations, salt, hash = encoded.split('$', 3)
+        if int(iterations) == 180000:  # Django 3.0.6
+            extra_iterations = self.iterations - int(iterations)
+            return extra_iterations < 0  # Can harden_runtime
+        return int(iterations) != self.iterations
+
+    PBKDF2PasswordHasher.must_update = must_update
