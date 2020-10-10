ad5e79227b0400882863def19a4d40eb59a6887d
Uri <uri@speedy.net>
2020-10-10 08:32:41 +0300

    Temp commit.

--- speedy/core/messages/admin.py
+++ speedy/core/messages/admin.py
@@ -1,6 +1,12 @@
+from django.contrib import admin as django_admin
+
 from speedy.core import admin
 from speedy.core.base.admin import ReadOnlyModelAdmin
 from .models import Chat, Message
+
+
+class MessageInlineAdmin(django_admin.TabularInline):
+    model = Message
 
 
 admin.site.register(Chat, ReadOnlyModelAdmin)
