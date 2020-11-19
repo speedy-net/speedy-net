86f4835549c51b91b1310673dd7ad9741cc620d5
Uri <uri@speedy.net>
2020-11-19 19:32:37 +0200

    If the user signed up to Speedy Net more than 10 days ago, and sent at least 20 messages with an email address without a reply in the last day, or at least 60 messages with an email address without a reply in the last 3 days, or at least 60 messages with an email address without a reply in the last week, then don't let them send any new messages to new chats.

--- speedy/core/messages/rules.py
+++ speedy/core/messages/rules.py
@@ -28,32 +28,39 @@ def can_send_new_message(user):
     then don't let them send any new messages to new chats.
     """
     if ((now() - user.date_created).days < 10):
-        count_user_messages_1_day = Chat.objects.count_chats_with_string_in_messages_and_only_one_sender(
-            entity=user,
-            string_in_messages="@",
-            created_after=now() - timedelta(days=1),
-        )
-        count_user_messages_3_days = Chat.objects.count_chats_with_string_in_messages_and_only_one_sender(
-            entity=user,
-            string_in_messages="@",
-            created_after=now() - timedelta(days=3),
-        )
-        count_user_messages_7_days = Chat.objects.count_chats_with_string_in_messages_and_only_one_sender(
-            entity=user,
-            string_in_messages="@",
-            created_after=now() - timedelta(days=7),
-        )
-        if ((count_user_messages_1_day >= 5) or (count_user_messages_3_days >= 10) or (count_user_messages_7_days >= 15)):
-            site = Site.objects.get_current()
-            logger.warning("User {user} can't send messages today on {site_name} ({count_user_messages_1_day} / {count_user_messages_3_days} / {count_user_messages_7_days}, registered {registered_days_ago} days ago).".format(
-                user=user,
-                site_name=_(site.name),
-                count_user_messages_1_day=count_user_messages_1_day,
-                count_user_messages_3_days=count_user_messages_3_days,
-                count_user_messages_7_days=count_user_messages_7_days,
-                registered_days_ago=(now() - user.date_created).days,
-            ))
-            return False
+        limit_user_messages_1_day = 5
+        limit_user_messages_3_days = 10
+        limit_user_messages_7_days = 15
+    else:
+        limit_user_messages_1_day = 20
+        limit_user_messages_3_days = 60
+        limit_user_messages_7_days = 60
+    count_user_messages_1_day = Chat.objects.count_chats_with_string_in_messages_and_only_one_sender(
+        entity=user,
+        string_in_messages="@",
+        created_after=now() - timedelta(days=1),
+    )
+    count_user_messages_3_days = Chat.objects.count_chats_with_string_in_messages_and_only_one_sender(
+        entity=user,
+        string_in_messages="@",
+        created_after=now() - timedelta(days=3),
+    )
+    count_user_messages_7_days = Chat.objects.count_chats_with_string_in_messages_and_only_one_sender(
+        entity=user,
+        string_in_messages="@",
+        created_after=now() - timedelta(days=7),
+    )
+    if ((count_user_messages_1_day >= limit_user_messages_1_day) or (count_user_messages_3_days >= limit_user_messages_3_days) or (count_user_messages_7_days >= limit_user_messages_7_days)):
+        site = Site.objects.get_current()
+        logger.warning("User {user} can't send messages today on {site_name} ({count_user_messages_1_day} / {count_user_messages_3_days} / {count_user_messages_7_days}, registered {registered_days_ago} days ago).".format(
+            user=user,
+            site_name=_(site.name),
+            count_user_messages_1_day=count_user_messages_1_day,
+            count_user_messages_3_days=count_user_messages_3_days,
+            count_user_messages_7_days=count_user_messages_7_days,
+            registered_days_ago=(now() - user.date_created).days,
+        ))
+        return False
     return True
 
 
