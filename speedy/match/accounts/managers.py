1cbe7db5abad62f69935794c2af8708b06f33fa8
Uri <uri@speedy.net>
2020-11-30 06:41:53 +0200

    Updated user_last_visit_days_offset in sorting of matches_list in get_matches.

--- speedy/match/accounts/managers.py
+++ speedy/match/accounts/managers.py
@@ -152,7 +152,15 @@ class SiteProfileManager(BaseManager):
                 elif (s in {25, 49, 73}):  # 3/77
                     other_user.speedy_match_profile._user_last_visit_days_offset -= 2 * 30
                 else:  # 71/77
-                    other_user.speedy_match_profile._user_last_visit_days_offset -= 0 * 30
+                    if ((timezone_now - other_user.speedy_match_profile.last_visit).days < 5):
+                        other_user.speedy_match_profile._user_last_visit_days_offset -= 0 * 30
+                    else:
+                        if (s in {27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 50, 51}):  # 23/77
+                            other_user.speedy_match_profile._user_last_visit_days_offset += 1 * 30
+                        elif (s in {52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 74, 75, 76}):  # 23/77
+                            other_user.speedy_match_profile._user_last_visit_days_offset += 2 * 30
+                        else:  # 25/77
+                            other_user.speedy_match_profile._user_last_visit_days_offset -= 0 * 30
                 matches_list.append(other_user)
         if (not (len(matches_list) == len(user_list))):
             # This is an error. All users should have ranks more than 0.
