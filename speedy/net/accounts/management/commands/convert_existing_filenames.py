f026eaf583398e260d1ef1b187f7534c6bb6b716
Aaron Chong <aaronjhchong@hotmail.com>
2020-12-18 15:36:06 +0800

    Implement command to convert existing filenames

---
+++ speedy/net/accounts/management/commands/convert_existing_filenames.py
@@ -0,0 +1,29 @@
+import logging
+import os
+
+from django.core.management import BaseCommand
+
+from speedy.core.uploads.models import File
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        files = File.objects.all().order_by('date_created')
+        for file in files:
+            old_path = file.file.path
+            new_filename = file.file.field.generate_filename(file, file.file.name)
+            file.file.name = new_filename
+            new_path = file.file.path
+            if old_path == new_path:
+                logger.debug('Skipped moving file {id}.'.format(id=file.id))
+                continue
+
+            logger.info('Moving file {id} from {old_path} to {new_path}.'.format(
+                id=file.id,
+                old_path=old_path,
+                new_path=file.file.path,
+            ))
+            os.rename(old_path, new_path)
+            file.save()
