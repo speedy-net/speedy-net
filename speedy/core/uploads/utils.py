456c40a028887780d1e11c5d6b16e3c69df9e078
Aaron Chong <aaronjhchong@hotmail.com>
2020-12-18 13:50:55 +0800

    Change filename to be based on ID of file object

--- speedy/core/uploads/utils.py
+++ speedy/core/uploads/utils.py
@@ -1,5 +1,12 @@
+import hashlib
+import os
+
+
 def uuid_dir(instance, filename):
     str_id = str(instance.id)
+    stem = hashlib.md5('$$$-{}-$$$'.format(instance.id).encode('utf-8')).hexdigest()
+    _, extension = os.path.splitext(filename)
+    filename = '{}{}'.format(stem, extension.lower())
     return '/'.join([
         str_id[:1],
         str_id[:2],
