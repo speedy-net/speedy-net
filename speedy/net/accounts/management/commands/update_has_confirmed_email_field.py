26176c8f03899294c87ff5f33306c34e124773b3
Uri <uri@speedy.net>
2020-12-24 18:34:49 +0200

    Updated the update_has_confirmed_email_field command.

--- speedy/net/accounts/management/commands/update_has_confirmed_email_field.py
+++ speedy/net/accounts/management/commands/update_has_confirmed_email_field.py
@@ -11,6 +11,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         users = User.objects.all()
         for user in users:
+            # Users might have changed in the database, load them again.
+            user = User.objects.get(pk=user.pk)
             user._update_has_confirmed_email_field()
 
 
