8c4d3805375dbf2dd63a23179b123f5c6e9c4217
Uri <uri@speedy.net>
2021-08-07 07:36:04 +0300

    Updated code.

--- speedy/match/accounts/managers.py
+++ speedy/match/accounts/managers.py
@@ -165,7 +165,7 @@ class SiteProfileManager(BaseManager):
                 # Generate a random number which changes every 4 hours, but doesn't change when reloading the page.
                 s = int(hashlib.md5("$$$-{}-{}-{}-{}-$$$".format(user.id, other_user.id, today.isoformat(), (((datetime_now.hour // 4) + 1) * 92)).encode('utf-8')).hexdigest(), 16) % 1000
                 if (0 <= s < 120):  # 12/100
-                    if (0 <= s < 18):
+                    if (0 <= s < 12):
                         distance_offset = int((s % 3) * 2 / 10 * 6 * 30 + 0.5)
                     else:
                         distance_offset = int((s % 3 + 3) * 2 / 10 * 6 * 30 + 0.5)
