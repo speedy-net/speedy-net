63ea29b0a1e023ff4ccd53684349493da1eac832
Uri <uri@speedy.net>
2021-09-02 01:17:23 +0300

    Updated admin.

--- speedy/composer/accounts/admin.py
+++ speedy/composer/accounts/admin.py
@@ -1,6 +1,12 @@
 from speedy.core import admin
+from speedy.core.accounts.admin import SiteProfileBaseAdmin
 from speedy.composer.accounts.models import SiteProfile as SpeedyComposerSiteProfile
 
-admin.site.register(SpeedyComposerSiteProfile)
+
+class SpeedyComposerSiteProfileAdmin(SiteProfileBaseAdmin):
+    pass
+
+
+admin.site.register(SpeedyComposerSiteProfile, SpeedyComposerSiteProfileAdmin)
 
 
