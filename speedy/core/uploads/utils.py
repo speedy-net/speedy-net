4c84520f6f2dd7f670ca172096d17251605f8050
Aaron Chong <aaronjhchong@hotmail.com>
2021-11-23 19:08:54 +0800

    Extract generate_id_if_needed for use in uuid_dir

--- speedy/core/uploads/utils.py
+++ speedy/core/uploads/utils.py
@@ -3,6 +3,9 @@ import os
 
 
 def uuid_dir(instance, filename):
+    if (not (instance.id) and (hasattr(instance, 'generate_id_if_needed'))):
+        instance.generate_id_if_needed()
+
     str_id = str(instance.id)
     stem = hashlib.md5('$$$-{}-$$$'.format(instance.id).encode('utf-8')).hexdigest()
     _, extension = os.path.splitext(filename)
