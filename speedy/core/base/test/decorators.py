40c2fd81f817867701f16d0e7eeeb3a7b38eb137
Uri <uri@speedy.net>
2021-12-28 08:27:09 +0200

    Updated test decorators.
    https://www.python.org/dev/peps/pep-0008/
    Always use a def statement instead of an assignment statement that binds a lambda expression directly to an identifier.

--- speedy/core/base/test/decorators.py
+++ speedy/core/base/test/decorators.py
@@ -5,21 +5,33 @@ if (django_settings.TESTS):
 
     from speedy.core.base.test import tests_settings
 
-    conditional_test = lambda conditional_function: unittest.skipUnless(condition=conditional_function(), reason="This test is irrelevant in {}.".format(tests_settings.SITE_NAME_EN_DICT[django_settings.SITE_ID]))
 
-    exclude_on_site = lambda site_id: conditional_test(conditional_function=lambda: (not (django_settings.SITE_ID == site_id)))
+    def conditional_test(conditional_function):
+        return unittest.skipUnless(condition=conditional_function(), reason="This test is irrelevant in {}.".format(tests_settings.SITE_NAME_EN_DICT[django_settings.SITE_ID]))
+
+
+    def exclude_on_site(site_id):
+        return conditional_test(conditional_function=lambda: (not (django_settings.SITE_ID == site_id)))
+
+
+    def only_on_site(site_id):
+        return conditional_test(conditional_function=lambda: (django_settings.SITE_ID == site_id))
+
+
+    def only_on_sites(site_id_list):
+        return conditional_test(conditional_function=lambda: (django_settings.SITE_ID in site_id_list))
+
+
     exclude_on_speedy_net = exclude_on_site(site_id=django_settings.SPEEDY_NET_SITE_ID)
     exclude_on_speedy_match = exclude_on_site(site_id=django_settings.SPEEDY_MATCH_SITE_ID)
     exclude_on_speedy_composer = exclude_on_site(site_id=django_settings.SPEEDY_COMPOSER_SITE_ID)
     exclude_on_speedy_mail_software = exclude_on_site(site_id=django_settings.SPEEDY_MAIL_SOFTWARE_SITE_ID)
 
-    only_on_site = lambda site_id: conditional_test(conditional_function=lambda: (django_settings.SITE_ID == site_id))
     only_on_speedy_net = only_on_site(site_id=django_settings.SPEEDY_NET_SITE_ID)
     only_on_speedy_match = only_on_site(site_id=django_settings.SPEEDY_MATCH_SITE_ID)
     only_on_speedy_composer = only_on_site(site_id=django_settings.SPEEDY_COMPOSER_SITE_ID)
     only_on_speedy_mail_software = only_on_site(site_id=django_settings.SPEEDY_MAIL_SOFTWARE_SITE_ID)
 
-    only_on_sites = lambda site_id_list: conditional_test(conditional_function=lambda: (django_settings.SITE_ID in site_id_list))
     only_on_sites_with_login = only_on_sites(site_id_list=django_settings.SITES_WITH_LOGIN)
 
 
