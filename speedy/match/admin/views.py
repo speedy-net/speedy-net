8d272ee1c3cda0b76954b48081fccd8227f17fb2
Uri <uri@speedy.net>
2022-01-30 18:59:53 +0200

    Updated admin.

--- speedy/match/admin/views.py
+++ speedy/match/admin/views.py
@@ -295,9 +295,12 @@ class AdminMatchesListView(OnlyAdminMixin, generic.ListView):
                 total_percent_of_male_active_members_in_age_range = round(float(total_number_of_male_active_members_in_age_range) / float(total_number_of_active_members_in_age_range) * 100, 1) if (total_number_of_active_members_in_age_range > 0) else 0
                 total_percent_of_other_active_members_in_age_range = round(float(total_number_of_other_active_members_in_age_range) / float(total_number_of_active_members_in_age_range) * 100, 1) if (total_number_of_active_members_in_age_range > 0) else 0
                 if (total_number_of_active_members_in_age_range > 0):
-                    if ((total_percent_of_female_active_members_in_age_range < 35) or (total_percent_of_male_active_members_in_age_range < 35)):
+                    if ((total_percent_of_female_active_members_in_age_range < 40) or (total_percent_of_male_active_members_in_age_range < 40)):
                         if (total_number_of_active_members_in_age_range >= 20):
-                            total_number_of_active_members_text += '<span style="color: red;">'
+                            if ((total_percent_of_female_active_members_in_age_range < 35) or (total_percent_of_male_active_members_in_age_range < 35)):
+                                total_number_of_active_members_text += '<span style="color: red;">'
+                            else:
+                                total_number_of_active_members_text += '<span style="color: blue;">'
                         else:
                             total_number_of_active_members_text += '<span style="color: #b10515;">'
                     else:
@@ -359,9 +362,12 @@ class AdminMatchesListView(OnlyAdminMixin, generic.ListView):
                 total_percent_of_male_active_members_in_age_range_in_the_last_four_months = round(float(total_number_of_male_active_members_in_age_range_in_the_last_four_months) / float(total_number_of_active_members_in_age_range_in_the_last_four_months) * 100, 1) if (total_number_of_active_members_in_age_range_in_the_last_four_months > 0) else 0
                 total_percent_of_other_active_members_in_age_range_in_the_last_four_months = round(float(total_number_of_other_active_members_in_age_range_in_the_last_four_months) / float(total_number_of_active_members_in_age_range_in_the_last_four_months) * 100, 1) if (total_number_of_active_members_in_age_range_in_the_last_four_months > 0) else 0
                 if (total_number_of_active_members_in_age_range_in_the_last_four_months > 0):
-                    if ((total_percent_of_female_active_members_in_age_range_in_the_last_four_months < 35) or (total_percent_of_male_active_members_in_age_range_in_the_last_four_months < 35)):
+                    if ((total_percent_of_female_active_members_in_age_range_in_the_last_four_months < 40) or (total_percent_of_male_active_members_in_age_range_in_the_last_four_months < 40)):
                         if (total_number_of_active_members_in_age_range_in_the_last_four_months >= 20):
-                            total_number_of_active_members_text += '<span style="color: red;">'
+                            if ((total_percent_of_female_active_members_in_age_range_in_the_last_four_months < 35) or (total_percent_of_male_active_members_in_age_range_in_the_last_four_months < 35)):
+                                total_number_of_active_members_text += '<span style="color: red;">'
+                            else:
+                                total_number_of_active_members_text += '<span style="color: blue;">'
                         else:
                             total_number_of_active_members_text += '<span style="color: #b10515;">'
                     else:
