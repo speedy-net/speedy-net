c7fb9eec08869a75596106f03f99b73af56c9000
Uri <uri@speedy.net>
2022-07-20 11:54:27 +0300

    Updated tests.

--- speedy/core/base/tests/test_middleware.py
+++ speedy/core/base/tests/test_middleware.py
@@ -15,4 +15,16 @@ if (django_settings.TESTS):
             self.assertEqual(first=RemoveExtraSlashesMiddleware.normalize_path(path='/zzz//yyy/'), second='/zzz/yyy/')
             self.assertEqual(first=RemoveExtraSlashesMiddleware.normalize_path(path='////xxx////yyy////'), second='/xxx/yyy/')
 
+        def test_redirect_to_url_without_extra_slashes(self):
+            # ~~~~ TODO: fix this test!
+            r = self.client.get(path='/about/')
+            self.assertEqual(first=r.status_code, second=200)
+            r = self.client.get(path='///about/')
+            self.assertEqual(first=r.status_code, second=200)  # ~~~~ TODO: should be 301
+            # self.assertRedirects(response=r, expected_url='/about/', status_code=301, target_status_code=200)
+            r = self.client.get(path='/about///')
+            self.assertRedirects(response=r, expected_url='/about/', status_code=301, target_status_code=200)
+            r = self.client.get(path='///about///')
+            self.assertRedirects(response=r, expected_url='/about/', status_code=301, target_status_code=200)
+
 
