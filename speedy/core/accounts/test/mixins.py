e72c49ad45513cbee7b3300204b9a3cc9d197e4b
Uri <uri@speedy.net>
2022-09-08 10:37:00 +0300

    Temp Commit.

--- speedy/core/accounts/test/mixins.py
+++ speedy/core/accounts/test/mixins.py
@@ -326,7 +326,7 @@ if (django_settings.TESTS):
             _username_is_required_error_message_dict = {'en': 'Username is required.', 'he': 'שם המשתמש/ת נדרש.'}
             _password_reset_on_speedy_net_subject_dict = {'en': "Password reset on Speedy Net", 'he': "איפוס סיסמה בספידי נט"}
             _password_reset_on_speedy_match_subject_dict = {'en': "Password reset on Speedy Match", 'he': "איפוס סיסמה בספידי מץ'"}
-            _ensure_this_value_is_greater_than_or_equal_to_minus_32768_error_message_dict = {'en': 'Ensure this value is greater than or equal to -32768.', 'he': 'Ensure this value is greater than or equal to -32768.'}
+            _ensure_this_value_is_greater_than_or_equal_to_minus_32768_error_message_dict = {'en': 'Ensure this value is greater than or equal to -32768.', 'he': 'יש לוודא שהערך גדול מ או שווה ל־-32768.'}
             _ensure_this_value_is_less_than_or_equal_to_32767_error_message_dict = {'en': 'Ensure this value is less than or equal to 32767.', 'he': 'Ensure this value is less than or equal to 32767.'}
 
             _value_is_not_a_valid_choice_error_message_to_format_dict = {'en': 'Value {value} is not a valid choice.', 'he': 'ערך {value} אינו אפשרות חוקית.'}
