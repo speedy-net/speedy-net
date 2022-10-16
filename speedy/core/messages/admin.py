0e4245ea7e800f3101e8c39a8b8bd40d7456b173
Aaron Chong <aaron@speedy.net>
2022-10-16 22:34:07 +0800

    Prefetch in MessageInlineAdmin for performance

--- speedy/core/messages/admin.py
+++ speedy/core/messages/admin.py
@@ -7,6 +7,16 @@ class MessageInlineAdmin(ReadOnlyTabularInlinePaginatedModelAdmin):
     model = Message
     per_page = 100
 
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related(
+            'chat__ent1__user__speedy_match_site_profile',
+            'chat__ent1__user__speedy_net_site_profile',
+            'chat__ent2__user__speedy_match_site_profile',
+            'chat__ent2__user__speedy_net_site_profile',
+            'chat__messages__sender',
+            'sender',
+        )
+
 
 admin.site.register(Chat, ReadOnlyModelAdmin)
 admin.site.register(Message, ReadOnlyModelAdmin2000)
