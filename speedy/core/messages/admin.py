b8b722caabc00b210cad83cd035d6cd3425566ec
Uri <uri@speedy.net>
2022-10-17 06:01:34 +0300

    Call function parameters by name.

--- speedy/core/messages/admin.py
+++ speedy/core/messages/admin.py
@@ -8,7 +8,7 @@ class MessageInlineAdmin(ReadOnlyTabularInlinePaginatedModelAdmin):
     per_page = 100
 
     def get_queryset(self, request):
-        return super().get_queryset(request).prefetch_related(
+        return super().get_queryset(request=request).prefetch_related(
             'chat__ent1__user__speedy_match_site_profile',
             'chat__ent1__user__speedy_net_site_profile',
             'chat__ent2__user__speedy_match_site_profile',
