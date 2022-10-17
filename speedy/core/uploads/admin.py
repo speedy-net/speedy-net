2ffe5b153449f79ae1ee28af517ef758da19f521
Uri <uri@speedy.net>
2022-10-17 07:54:25 +0300

    Updated admin.

--- speedy/core/uploads/admin.py
+++ speedy/core/uploads/admin.py
@@ -3,7 +3,22 @@ from speedy.core.base.admin import ReadOnlyModelAdmin5000
 from .models import File, Image
 
 
-admin.site.register(File, ReadOnlyModelAdmin5000)
-admin.site.register(Image, ReadOnlyModelAdmin5000)
+class FileOwnerAdminMixin(object):
+    def get_queryset(self, request):
+        return super().get_queryset(request=request).prefetch_related(
+            'owner',
+        )
+
+
+class FileAdmin(FileOwnerAdminMixin, ReadOnlyModelAdmin5000):
+    pass
+
+
+class ImageAdmin(FileOwnerAdminMixin, ReadOnlyModelAdmin5000):
+    pass
+
+
+admin.site.register(File, FileAdmin)
+admin.site.register(Image, ImageAdmin)
 
 
