53b252acce24ce6ea8c6077d957cbefe2988408a
Uri <uri@speedy.net>
2022-10-17 16:59:48 +0300

    ReadOnlyModelAdminMixin - removed unneeded code.

--- speedy/core/base/admin.py
+++ speedy/core/base/admin.py
@@ -25,13 +25,6 @@ class ReadOnlyModelAdminMixin(object):
     """
     actions = None
 
-    # We cannot call super().get_fields(request, obj) because that method calls
-    # get_readonly_fields(request, obj), causing infinite recursion. Ditto for
-    # super().get_form(request, obj). So we assume the default ModelForm.
-    def get_readonly_fields(self, request, obj=None):
-        return self.fields or [f.name for f in (self.model._meta.fields + self.model._meta.many_to_many)]
-
-    # Allow viewing objects but not actually changing them.
     def has_change_permission(self, request, obj=None):
         return False
 
