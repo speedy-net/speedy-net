7a9e307d53fd9084227210dbe92d0b6b21bb6f6b
Uri <uri@speedy.net>
2022-10-20 04:42:33 +0300

    Updated tests.

--- speedy/core/base/test/mixins.py
+++ speedy/core/base/test/mixins.py
@@ -17,7 +17,7 @@ if (django_settings.TESTS):
             _this_field_is_required_error_message_dict = {'en': 'This field is required.', 'he': 'יש להזין תוכן בשדה זה.'}
             # _ensure_this_value_has_at_least_min_length_characters_error_message_to_format_dict = {'en': 'Ensure this value has at least {min_length} characters (it has {value_length}).', 'he': '_____ # ~~~~ TODO'} # ~~~~ TODO
             _ensure_this_value_has_at_most_max_length_characters_error_message_to_format_dict = {'en': 'Ensure this value has at most {max_length} characters (it has {value_length}).', 'he': 'נא לוודא שערך זה מכיל {max_length} תווים לכל היותר (מכיל {value_length}).'}
-            _exceeds_the_limit_4300_for_integer_string_conversion_error_message_to_format_dict = {'en': 'Exceeds the limit (4300) for integer string conversion: value has {digits} digits', 'he': 'Exceeds the limit (4300) for integer string conversion: value has {digits} digits'}
+            _exceeds_the_limit_4300_for_integer_string_conversion_error_message_to_format_dict = {'en': 'Exceeds the limit (4300) for integer string conversion: value has {digits} digits; use sys.set_int_max_str_digits() to increase the limit', 'he': 'Exceeds the limit (4300) for integer string conversion: value has {digits} digits; use sys.set_int_max_str_digits() to increase the limit'}
 
             self._this_field_is_required_error_message = _this_field_is_required_error_message_dict[self.language_code]
             # self._ensure_this_value_has_at_least_min_length_characters_error_message_to_format = _ensure_this_value_has_at_least_min_length_characters_error_message_to_format_dict[self.language_code]
