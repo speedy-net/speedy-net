dd66f6d6070b322311661a2d2a6a3cf72a0ced5c
Uri <uri@speedy.net>
2023-07-05 11:48:21 +0300

    Added 4 more languages to Speedy Net and Speedy Match - not everything is working yet:
        ('nl', _('Dutch')),
        ('sv', _('Swedish')),
        ('ko', _('Korean')),
        ('fi', _('Finnish')),

--- speedy/core/base/utils.py
+++ speedy/core/base/utils.py
@@ -219,7 +219,7 @@ def get_all_field_names(base_field_name):
     #     base_field_name=base_field_name,
     #     field_names=field_names,
     # ))
-    assert (len(field_names) == 3)
+    assert (len(field_names) == 11)
     return field_names
 
 
