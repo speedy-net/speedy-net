59192bc11836e280fd08960f1fdeb3e0271b5c6e
Uri <uri@speedy.net>
2023-09-05 09:22:05 +0300

    Added 4 more languages to Speedy Net and Speedy Match - not everything is working yet:
        ('de', _('German')),
        ('es', _('Spanish')),
        ('pt', _('Portuguese')),
        ('it', _('Italian')),

---
+++ speedy/core/formats/pt/formats.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+
+DATE_FORMAT = r"j \d\e F \d\e Y"
+MONTH_DAY_FORMAT = r"j \d\e F"
+
+
+DATE_INPUT_FORMATS = [
+    "%Y-%m-%d",                           # '2006-10-25',
+]
+
+
