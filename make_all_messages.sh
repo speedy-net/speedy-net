27b07a9f64968cc9265e2e630cea500ca5d22369
Uri <uri@speedy.net>
2023-09-24 17:08:34 +0300

    make_all_messages.sh - use `make_messages --all` instead of specific locales.

--- make_all_messages.sh
+++ make_all_messages.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-./manage_all_sites_with_all_warnings.sh make_messages --locale en --locale fr --locale de --locale es --locale pt --locale it --locale he
+./manage_all_sites_with_all_warnings.sh make_messages --all
 
