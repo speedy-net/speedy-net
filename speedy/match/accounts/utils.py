d0f7aa6202b59f9f55c312b1ca33d79caa82d160
Uri <uri@speedy.net>
2023-11-29 14:27:29 +0200

    Localization: use formats.number_format instead of commas as thousand separators in numbers.

--- speedy/match/accounts/utils.py
+++ speedy/match/accounts/utils.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 
+from django.utils import formats
 from django.utils.timezone import now
 from django.utils.translation import gettext_lazy as _
 
@@ -86,8 +87,8 @@ def get_total_number_of_active_members_text():
     # We only display this information on the website if the numbers are at least 300 and 50.
     if ((total_number_of_active_members_in_the_last_four_months >= 300) and (total_number_of_active_members_in_the_last_week >= 50)):
         total_number_of_active_members_text = _("The total number of active members on the site is {total_number_of_active_members_in_the_last_four_months}, of which {total_number_of_active_members_in_the_last_week} members entered the site in the last week.").format(
-            total_number_of_active_members_in_the_last_four_months='{:,}'.format(total_number_of_active_members_in_the_last_four_months),
-            total_number_of_active_members_in_the_last_week='{:,}'.format(total_number_of_active_members_in_the_last_week),
+            total_number_of_active_members_in_the_last_four_months=formats.number_format(value=total_number_of_active_members_in_the_last_four_months),
+            total_number_of_active_members_in_the_last_week=formats.number_format(value=total_number_of_active_members_in_the_last_week),
         )
     else:
         total_number_of_active_members_text = ""
