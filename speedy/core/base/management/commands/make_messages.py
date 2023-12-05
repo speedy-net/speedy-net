7346f30d3500336ef5b5005ea27ee22569a21ae2
Bram <78353155+pescheck-bram@users.noreply.github.com>
2023-12-05 18:01:57 +0100

    Update make_messages.py

--- speedy/core/base/management/commands/make_messages.py
+++ speedy/core/base/management/commands/make_messages.py
@@ -1,7 +1,12 @@
 from django.core.management.commands import makemessages
 
-
 class Command(makemessages.Command):
-    msgmerge_options = makemessages.Command.msgmerge_options + ["--no-fuzzy-matching", ]
+    msgmerge_options = makemessages.Command.msgmerge_options + ["--no-fuzzy-matching"]
 
-
+    def add_arguments(self, parser):
+        super().add_arguments(parser)
+        parser.add_argument(
+            '--no-fuzzy-matching',
+            action='store_true',
+            help='Do not use fuzzy matching in msgmerge.',
+        )
