05d741c44e7db5a2de0f07b333c9996aea5d2e6c
Uri <uri@speedy.net>
2023-12-07 16:10:25 +0200

    send_confirmation_reminders - send the confirmation email in the language the user is active or completed step 6.

--- speedy/net/accounts/management/commands/send_confirmation_reminders.py
+++ speedy/net/accounts/management/commands/send_confirmation_reminders.py
@@ -1,10 +1,13 @@
 import logging
 from datetime import timedelta
 
+from django.conf import settings as django_settings
 from django.core.management import BaseCommand
+from django.utils import translation
 from django.utils.timezone import now
 
 from speedy.core.accounts.models import UserEmailAddress
+from speedy.core.base.utils import to_attribute
 
 logger = logging.getLogger(__name__)
 
@@ -13,10 +16,22 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         emails = UserEmailAddress.objects.filter(is_confirmed=False, date_created__lte=(now() - timedelta(days=5)), confirmation_sent__lte=1).exclude(confirmation_token='')
         for email in emails:
+            translation.activate(language='en')
+            language_code_found = False
             user = email.user
             if (email.date_created <= now() - timedelta(days=5)):
                 if ((email.is_confirmed is False) and (email.confirmation_sent <= 1)):
                     if (user.is_active):
+                        for language_code, language_name in django_settings.LANGUAGES:
+                            if (not language_code_found):
+                                if (language_code in user.speedy_match_profile.active_languages):
+                                    translation.activate(language=language_code)
+                                    language_code_found = True
+                        for language_code, language_name in django_settings.LANGUAGES:
+                            if (not language_code_found):
+                                if getattr(user.speedy_match_profile, to_attribute(name='activation_step', language_code=language_code), 0) >= 7:
+                                    translation.activate(language=language_code)
+                                    language_code_found = True
                         logger.debug("Sending confirmation to email {email} of user {user}. Confirmation sent {confirmation_sent} times, Added on {date_created} (registered {registered_days_ago} days ago).".format(
                             email=email,
                             user=user,
