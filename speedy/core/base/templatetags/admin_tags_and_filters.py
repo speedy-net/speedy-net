856a46942f6311b5b11d89cb58e744e72159552d
Uri <uri@speedy.net>
2023-12-19 09:09:40 +0200

    Updated admin - added default_value, don't display value if equal to default_value.

--- speedy/core/base/templatetags/admin_tags_and_filters.py
+++ speedy/core/base/templatetags/admin_tags_and_filters.py
@@ -6,11 +6,11 @@ from speedy.core.base.utils import to_attribute
 register = template.Library()
 
 
-def attribute_html(profile, attribute_name):
+def attribute_html(profile, attribute_name, default_value):
     attribute_list = []
     for language_code, language_name in django_settings.LANGUAGES:
         attribute_lang = getattr(profile, to_attribute(name=attribute_name, language_code=language_code), None)
-        if (not (attribute_lang is None)):
+        if ((not (attribute_lang is None)) and (not (attribute_lang == default_value))):
             attribute_list.append("'{}':{}".format(language_code, attribute_lang))
     if (len(attribute_list) > 0):
         return "({})".format(", ".join(attribute_list))
@@ -20,16 +20,16 @@ def attribute_html(profile, attribute_name):
 
 @register.filter
 def activation_step_html(speedy_match_profile):
-    return attribute_html(profile=speedy_match_profile, attribute_name="activation_step")
+    return attribute_html(profile=speedy_match_profile, attribute_name="activation_step", default_value=2)
 
 
 @register.filter
 def number_of_matches_html(speedy_match_profile):
-    return attribute_html(profile=speedy_match_profile, attribute_name="number_of_matches")
+    return attribute_html(profile=speedy_match_profile, attribute_name="number_of_matches", default_value=None)
 
 
 @register.filter
 def number_of_friends_html(speedy_net_profile):
-    return attribute_html(profile=speedy_net_profile, attribute_name="number_of_friends")
+    return attribute_html(profile=speedy_net_profile, attribute_name="number_of_friends", default_value=None)
 
 
