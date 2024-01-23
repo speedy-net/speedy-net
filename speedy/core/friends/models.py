4a450879e94f2d9e50dda529f8d72ac3cb0a143c
Aaron Chong <aaron@speedy.net>
2024-01-23 23:03:59 +0800

    Invalidate received_friendship_requests_count

--- speedy/core/friends/models.py
+++ speedy/core/friends/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.dispatch import receiver
-from friendship.models import Friend
+from friendship.models import Friend, FriendshipRequest
 
+from speedy.core.accounts.cache_helper import bust_cache_by_keys, get_keys_for_bust_cache
 from speedy.net.accounts.models import SiteProfile as SpeedyNetSiteProfile
 
 
@@ -19,4 +20,17 @@ def update_friends_count_on_unfriend(sender, instance: Friend, **kwargs):
     # Do .filter(...).update(...) because for cascade delete User -> Friend, accessing user.speedy_net_profile will re-create deleted SpeedyNetSiteProfile
     SpeedyNetSiteProfile.objects.filter(user=user).update(friends_count=user.friends.count())
 
+
+@receiver(signal=models.signals.post_save, sender=FriendshipRequest)
+def invalidate_received_friendship_requests_count_after_friendship_request_created(sender, instance: FriendshipRequest, **kwargs):
+    keys1 = get_keys_for_bust_cache(cache_type='received_friendship_requests_count', entity_pk=instance.from_user.pk)
+    keys2 = get_keys_for_bust_cache(cache_type='received_friendship_requests_count', entity_pk=instance.to_user.pk)
+    bust_cache_by_keys(cache_keys=keys1 + keys2)
+
+
+@receiver(signal=models.signals.post_delete, sender=FriendshipRequest)
+def invalidate_received_friendship_requests_count_after_friendship_request_deleted(sender, instance: FriendshipRequest, **kwargs):
+    keys1 = get_keys_for_bust_cache(cache_type='received_friendship_requests_count', entity_pk=instance.from_user.pk)
+    keys2 = get_keys_for_bust_cache(cache_type='received_friendship_requests_count', entity_pk=instance.to_user.pk)
+    bust_cache_by_keys(cache_keys=keys1 + keys2)
 
