627aad16d705fd530cdb5ea2f9245221aa28735a
Uri <uri@speedy.net>
2024-01-27 08:23:05 +0200

    Patched friendship_patches.py.

--- speedy/core/patches/friendship_patches.py
+++ speedy/core/patches/friendship_patches.py
@@ -1,6 +1,23 @@
 def patch():
     from friendship.models import FriendshipManager, FriendshipRequest, cache, cache_key
 
+    def requests(self, user):
+        """ Return a list of friendship requests """
+        key = cache_key("requests", user.pk)
+        requests = cache.get(key)
+
+        if requests is None:
+            qs = (
+                # FriendshipRequest.objects.select_related("from_user", "to_user")
+                FriendshipRequest.objects
+                .filter(to_user=user)
+                .all()
+            )
+            requests = list(qs)
+            cache.set(key, requests)
+
+        return requests
+
     def sent_requests(self, user):
         """ Return a list of friendship requests from user """
         key = cache_key("sent_requests", user.pk)
@@ -18,6 +35,7 @@ def patch():
 
         return requests
 
+    FriendshipManager.requests = requests
     FriendshipManager.sent_requests = sent_requests
 
 
