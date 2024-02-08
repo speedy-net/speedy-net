4685a5982f600efb0b20eba5a02dfd889eff50ce
Uri <uri@speedy.net>
2024-02-08 21:46:39 +0200

    Temp commit

--- speedy/core/admin/views.py
+++ speedy/core/admin/views.py
@@ -126,10 +126,11 @@ class AdminUsersListView(OnlyAdminMixin, generic.ListView):
 
     def get_queryset(self):
         SiteProfile = get_site_profile_model()
+        order_by_list = list()
         if (self.request.GET.get('order_by') == 'number_of_friends'):
-            qs = User.objects.all().order_by(F('{}__{}'.format(SpeedyNetSiteProfile.RELATED_NAME, to_attribute(name='number_of_friends'))).desc(nulls_last=True), '-{}__last_visit'.format(SiteProfile.RELATED_NAME))
-        else:
-            qs = User.objects.all().order_by('-{}__last_visit'.format(SiteProfile.RELATED_NAME))
+            order_by_list.append(F('{}__{}'.format(SpeedyNetSiteProfile.RELATED_NAME, to_attribute(name='number_of_friends'))).desc(nulls_last=True))
+        order_by_list.append('-{}__last_visit'.format(SiteProfile.RELATED_NAME))
+        qs = User.objects.all().order_by(*order_by_list)
         return qs
 
     def get_context_data(self, **kwargs):
