850d4fd6cf6cfb7c5658974f7333ac04777ca197
Uri <uri@speedy.net>
2024-02-09 12:05:23 +0200

    Temp commit

--- speedy/net/accounts/forms.py
+++ speedy/net/accounts/forms.py
@@ -1,31 +1,15 @@
 import logging
 
-# from crispy_forms.bootstrap import InlineField
-from crispy_forms.layout import Submit, Div, HTML, Row, Hidden, Layout
+from crispy_forms.layout import Submit
 
 from django import forms
 from django.conf import settings as django_settings
-# from django.contrib.auth import forms as django_auth_forms, password_validation
 from django.contrib.sites.models import Site
-# from django.urls import reverse
-# from django.utils.timezone import now
-from django.utils.translation import get_language, gettext_lazy as _, pgettext_lazy
+from django.utils.translation import gettext_lazy as _, pgettext_lazy
 from django.core.exceptions import ValidationError
-# from django.contrib.auth.tokens import default_token_generator
-# from django.contrib.sites.shortcuts import get_current_site
-# from django.utils.encoding import force_bytes
-# from django.utils.http import urlsafe_base64_encode
-# from django.template.loader import render_to_string
 
-from speedy.core.base.forms import ModelFormWithDefaults, FormHelperWithDefaults
+from speedy.core.base.forms import FormHelperWithDefaults
 from speedy.core.accounts.forms import AddAttributesToFieldsMixin
-# from speedy.core.accounts.utils import get_site_profile_model
-# from speedy.core.base.mail import send_mail
-# from speedy.core.base.utils import normalize_username, to_attribute
-# from speedy.core.uploads.models import Image
-# from .models import User, UserEmailAddress
-# from .utils import normalize_email
-# from . import validators as speedy_core_accounts_validators
 
 logger = logging.getLogger(__name__)
 
