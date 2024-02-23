647b2962757d595dde9844d1ead871f18d65c94c
Uri <uri@speedy.net>
2024-02-23 02:51:43 +0200

    Added a message in Speedy Composer and Speedy Mail Software: "This site is under construction" - translated to Hebrew and English.

--- speedy/core/base/context_processors.py
+++ speedy/core/base/context_processors.py
@@ -17,7 +17,7 @@ def active_url_name(request):
 
 def settings(request):
     settings_in_templates = {}
-    for attr in ["SITE_ID", "SPEEDY_NET_SITE_ID", "SPEEDY_MATCH_SITE_ID", "SPEEDY_COMPOSER_SITE_ID", "SPEEDY_MAIL_SOFTWARE_SITE_ID", "XD_AUTH_SITES", "LANGUAGES_WITH_ADS"]:
+    for attr in ["SITE_ID", "SPEEDY_NET_SITE_ID", "SPEEDY_MATCH_SITE_ID", "SPEEDY_COMPOSER_SITE_ID", "SPEEDY_MAIL_SOFTWARE_SITE_ID", "XD_AUTH_SITES", "LANGUAGES_WITH_ADS", "THIS_SITE_IS_UNDER_CONSTRUCTION"]:
         if (hasattr(django_settings, attr)):
             settings_in_templates[attr] = getattr(django_settings, attr)
     return {
--- speedy/core/settings/base_without_login.py
+++ speedy/core/settings/base_without_login.py
@@ -2,6 +2,7 @@ from .base import *
 
 
 LOGIN_ENABLED = False
+THIS_SITE_IS_UNDER_CONSTRUCTION = True
 
 INSTALLED_APPS += [
     'speedy.core.contact_by_email',
