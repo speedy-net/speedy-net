ef94efe1f3316a95c091e9fc78903e2251ad269a
Uri <uri@speedy.net>
2024-06-14 12:23:54 +0300

    Updated `def must_update()` in session_patches.

--- speedy/core/patches/session_patches.py
+++ speedy/core/patches/session_patches.py
@@ -3,10 +3,9 @@ from django.contrib.auth.hashers import PBKDF2PasswordHasher
 
 def patch():
     def must_update(self, encoded):
-        # Update the stored password only if the iterations diff is at least 340,000.
+        # Update the stored password only if the current iterations are less than or equal to 140,000.
         decoded = self.decode(encoded=encoded)
-        iterations_diff = abs(self.iterations - decoded["iterations"])
-        return ((decoded["iterations"] != self.iterations) and (iterations_diff >= 340000))
+        return ((decoded["iterations"] != self.iterations) and (decoded["iterations"] <= 140000))
 
     PBKDF2PasswordHasher.iterations = 480000
     PBKDF2PasswordHasher.must_update = must_update
