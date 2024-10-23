36d5b18ad77fc6e43973706158554b4a2feeb2d9
Uri <uri@speedy.net>
2024-10-23 16:14:09 +0300

    Revert PR.

--- speedy/core/base/management/commands/make_messages.py
+++ speedy/core/base/management/commands/make_messages.py
@@ -4,12 +4,4 @@ from django.core.management.commands import makemessages
 class Command(makemessages.Command):
     msgmerge_options = makemessages.Command.msgmerge_options + ["--no-fuzzy-matching"]
 
-    def add_arguments(self, parser):
-        super().add_arguments(parser=parser)
-        parser.add_argument(
-            "--no-fuzzy-matching",
-            action="store_true",
-            help="Do not use fuzzy matching in msgmerge.",
-        )
-
 
