9712a2c5990cf9f0a6e3c360fb9d570871833fc6
Aaron Chong <aaron@speedy.net>
2024-10-25 01:19:57 +0800

    Try/except MemcacheServerError and log warning

--- speedy/core/patches/friendship_patches.py
+++ speedy/core/patches/friendship_patches.py
@@ -1,11 +1,16 @@
 def patch():
+    import logging
+
     from friendship.models import BUST_CACHES, CACHE_TYPES, Friend, FriendshipManager, FriendshipRequest, cache, cache_key
+    from pymemcache.exceptions import MemcacheServerError
 
     MAX_NUMBER_OF_FRIENDS_TO_CACHE = 400
     MAX_NUMBER_OF_REQUESTS_TO_CACHE = 4000
 
     CACHE_TYPES.setdefault("friends_count", "speedy-core-friends-count-%s")
     BUST_CACHES["friends"].append("friends_count") if "friends_count" not in BUST_CACHES["friends"] else None
+
+    logger = logging.getLogger(__name__)
 
     def friends(self, user):
         """ Return a list of all friends """
@@ -16,7 +21,14 @@ def patch():
             qs = Friend.objects.select_related("from_user").filter(to_user=user)
             friends = [u.from_user for u in qs]
             if (len(friends) <= MAX_NUMBER_OF_FRIENDS_TO_CACHE):
-                cache.set(key, friends)
+                try:
+                    cache.set(key, friends)
+                except MemcacheServerError as e:
+                    logger.warning("friendship_patches::friends::User has {len_friends} friends, user={user}, Exception={e}".format(
+                        len_friends=len(friends),
+                        user=user,
+                        e=str(e),
+                    ))
 
         return friends
 
@@ -34,7 +46,14 @@ def patch():
             )
             requests = list(qs)
             if (len(requests) <= MAX_NUMBER_OF_REQUESTS_TO_CACHE):
-                cache.set(key, requests)
+                try:
+                    cache.set(key, requests)
+                except MemcacheServerError as e:
+                    logger.warning("friendship_patches::requests::User has {len_requests} requests, user={user}, Exception={e}".format(
+                        len_requests=len(requests),
+                        user=user,
+                        e=str(e),
+                    ))
 
         return requests
 
@@ -52,7 +71,14 @@ def patch():
             )
             requests = list(qs)
             if (len(requests) <= MAX_NUMBER_OF_REQUESTS_TO_CACHE):
-                cache.set(key, requests)
+                try:
+                    cache.set(key, requests)
+                except MemcacheServerError as e:
+                    logger.warning("friendship_patches::sent_requests::User has {len_requests} sent requests, user={user}, Exception={e}".format(
+                        len_requests=len(requests),
+                        user=user,
+                        e=str(e),
+                    ))
 
         return requests
 
