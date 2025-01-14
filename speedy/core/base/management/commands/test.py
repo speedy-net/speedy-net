5cad1738cbb1cef3c2a731f6d7f17acabe66310f
Uri <uri@speedy.net>
2025-01-14 18:08:06 +0200

    Implement --count-tests grouped by name of N depth

--- speedy/core/base/management/commands/test.py
+++ speedy/core/base/management/commands/test.py
@@ -45,4 +45,13 @@ class Command(test.Command):
             type=int,
         )
 
+        parser.add_argument(
+            "--count-tests",
+            action="store",
+            help="If run with this argument, count the tests grouped by test.id() parts of this depth. If passed without a number, defaults to 3.",
+            type=int,
+            nargs="?",
+            const=3,
+        )
+
 
