ff4910c69a51f0ae79385a6c0b7d9a2a8757a915
Uri <uri@speedy.net>
2025-03-15 03:48:43 +0200

    Updated code.

--- contrib/deploy.sh
+++ contrib/deploy.sh
@@ -10,7 +10,7 @@ git pull
 ${PY} -m pip install --upgrade pip
 ${PIP} install --upgrade -r requirements.txt -r production-requirements.txt
 ${PIP} uninstall -y psycopg-binary
-# ${PIP} install psycopg-c==3.2.5
+# ${PIP} install --upgrade psycopg-c==3.2.5
 ${PIP} uninstall -y django-debug-toolbar # django-debug-toolbar should never be installed in production or staging.
 ${PIP} uninstall -y factory-boy # factory-boy should never be installed in production or staging.
 ${PIP} uninstall -y Faker # Faker should never be installed in production or staging.
