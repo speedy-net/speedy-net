7e1889361f07b31800412398dfd36c36a3a3a82f
Uri <uri@speedy.net>
2025-05-02 07:37:11 +0300

    Removed commented code in speedy.core.messages.rules .

--- speedy/core/messages/rules.py
+++ speedy/core/messages/rules.py
@@ -51,26 +51,8 @@ def can_send_new_message(user):
                     email_name, domain_part = user.email.strip().rsplit("@", 1)
                     if (domain_part in {'gmail.com'}):
                         can_send = True
-    #     if ((now() - user.date_created).days < 2):
-    #         if (user.has_confirmed_email):
-    #             emails = user.email_addresses.filter(is_primary=True)
-    #             if ((len(emails) == 1) and (user.email) and (user.email == emails[0].email) and (emails[0].is_confirmed)):
-    #                 email_name, domain_part = user.email.strip().rsplit("@", 1)
-    #                 if (domain_part in {'outlook.com', 'hotmail.com'}):
-    #                     can_send = False
     if ((now() - user.date_created).days < 10):
         limit_user_messages_1_day, limit_user_messages_3_days, limit_user_messages_7_days = 5, 10, 15
-        # if ((now() - user.date_created).days < 3):
-        #     limit_user_messages_1_day, limit_user_messages_3_days, limit_user_messages_7_days = 2, 10, 15
-        #     if (user.has_confirmed_email):
-        #         emails = user.email_addresses.filter(is_primary=True)
-        #         if ((len(emails) == 1) and (user.email) and (user.email == emails[0].email) and (emails[0].is_confirmed)):
-        #             email_name, domain_part = user.email.strip().rsplit("@", 1)
-        #             if (domain_part in {'gmail.com'}):
-        #                 if ((now() - user.date_created).days < 2):
-        #                     limit_user_messages_1_day, limit_user_messages_3_days, limit_user_messages_7_days = 3, 10, 15
-        #                 else:
-        #                     limit_user_messages_1_day, limit_user_messages_3_days, limit_user_messages_7_days = 5, 10, 15
     else:
         limit_user_messages_1_day, limit_user_messages_3_days, limit_user_messages_7_days = 20, 60, 60
     strings_in_messages = ["@", "http://", "https://"]
