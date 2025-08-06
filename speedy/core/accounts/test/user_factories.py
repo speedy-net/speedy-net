0b43af8ed7cf3b84e0371d3bb1e994ef57f0e3a3
Uri <uri@speedy.net>
2025-08-06 07:44:41 +0300

    Updated tests.

--- speedy/core/accounts/test/user_factories.py
+++ speedy/core/accounts/test/user_factories.py
@@ -5,7 +5,9 @@ if (django_settings.TESTS):
         if (django_settings.SITE_ID == django_settings.SPEEDY_NET_SITE_ID):
             from speedy.net.accounts.test.user_factories import DefaultUserFactory, InactiveUserFactory, ActiveUserFactory
             from speedy.match.accounts.test.user_factories import SpeedyNetInactiveUserFactory
-        if (django_settings.SITE_ID == django_settings.SPEEDY_MATCH_SITE_ID):
+        elif (django_settings.SITE_ID == django_settings.SPEEDY_MATCH_SITE_ID):
             from speedy.match.accounts.test.user_factories import DefaultUserFactory, InactiveUserFactory, ActiveUserFactory, SpeedyNetInactiveUserFactory
+        else:
+            raise NotImplementedError("User factories are not implemented for this site.")
 
 
